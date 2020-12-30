d03311a52808949125390b914bd57eedc416469f
Marta Pancaldi <marta.panc@gmail.com>
2020-12-30 21:14:45 +0000

    AoC2017 D10P2: complete

--- src/main/kotlin/aoc2017/day10/Day10.kt
+++ src/main/kotlin/aoc2017/day10/Day10.kt
@@ -7,66 +7,60 @@ fun readInputToList(path: String): List<Int> {
     return line.split(",").map { it.toInt() }
 }
 
-fun processList(inputLength: List<Int>, sizeOfList: Int): Long {
-    var inputList = (0 until sizeOfList).toMutableList()
-    inputList = runRound(inputLength, inputList).outputList
+fun processListPart1(inputLength: List<Int>, listSize: Int): Long {
+    val inputList = knotHash(inputLength, 1, listSize)
     return inputList[0].toLong() * inputList[1]
 }
 
-private fun runRound(lengths: List<Int>, numbers: List<Int>, initIndex: Int = 0, initSkipVal: Int = 0): Day10 {
-    var index = initIndex
-    var skipValue = initSkipVal
-    var inputList = numbers.toMutableList()
-    for ((_, length) in lengths.withIndex()) {
-        if (index + length < inputList.size) {
-            val selection = inputList.subList(index, index + length).reversed().toMutableList()
-            for (value in selection) {
-                inputList.remove(value)
-            }
-            inputList.addAll(index, selection)
-        } else {
-            val splitSelection = inputList.subList(index, inputList.size).toMutableList()
-            val temp = inputList.subList(0, length - splitSelection.size)
-            splitSelection.addAll(temp)
-            splitSelection.reverse()
+fun processListPart2(inputLength: List<Int>, listSize: Int = 256): String {
+    val editedLengths = inputLength + listOf(17, 31, 73, 47, 23)
+    val output = knotHash(editedLengths, 64, listSize).toList()
+    val denseHashesList = output.chunked(16).map { computeXorOfList(it) }
+    return computeHexString(denseHashesList)
+}
 
-            val afterCurrentIndex = inputList.size - index
-            val beforeCurrentIndex = length - afterCurrentIndex
-            inputList = inputList.subList(0, index)
-            for (i in 0 until afterCurrentIndex) {
-                inputList.add(splitSelection.removeAt(0))
+private fun knotHash(lengths: List<Int>, rounds: Int = 64, listSize: Int = 256): List<Int> {
+    var index = 0
+    var skipValue = 0
+    var inputList = (0 until listSize).toMutableList()
+    repeat(rounds) {
+        for (length in lengths) {
+            if (index + length < inputList.size) {
+                val selection = inputList.subList(index, index + length).reversed().toMutableList()
+                for (value in selection) {
+                    inputList.remove(value)
+                }
+                inputList.addAll(index, selection)
+            } else {
+                val splitSelection = inputList.subList(index, inputList.size).toMutableList()
+                val temp = inputList.subList(0, length - splitSelection.size)
+                splitSelection.addAll(temp)
+                splitSelection.reverse()
+
+                val afterCurrentIndex = inputList.size - index
+                val beforeCurrentIndex = length - afterCurrentIndex
+                inputList = inputList.subList(0, index)
+                for (i in 0 until afterCurrentIndex) {
+                    inputList.add(splitSelection.removeAt(0))
+                }
+                inputList = inputList.subList(beforeCurrentIndex, inputList.size)
+                for (i in 0 until beforeCurrentIndex) {
+                    inputList.add(i, splitSelection[i])
+                }
+                inputList = inputList.toSet().toMutableList()
             }
-            inputList = inputList.subList(beforeCurrentIndex, inputList.size)
-            for (i in 0 until beforeCurrentIndex) {
-                inputList.add(i, splitSelection[i])
-            }
-            inputList = inputList.toSet().toMutableList()
+            index = (index + (length + skipValue)) % inputList.size
+            skipValue++
         }
-        index = (index + (length + skipValue)) % inputList.size
-        skipValue++
     }
-    return Day10(inputList, index, skipValue)
+    return inputList
 }
 
 fun convertListToASCII(input: String, isPath: Boolean = false): List<Int> {
     val asciiList = mutableListOf<Int>()
     val inputString = if (isPath) readInputLineByLine(input)[0] else input
-    for (char in inputString) {
-        asciiList.add(char.toInt())
-    }
+    for (char in inputString) asciiList.add(char.toInt())
     return asciiList
-}
-
-fun processListPart2(inputLength: List<Int>, sizeOfList: Int = 256): String {
-    val inputList = (0 until sizeOfList).toMutableList()
-    val editedInputLength = inputLength + listOf(17, 31, 73, 47, 23)
-    var output = runRound(editedInputLength, inputList)
-    for (i in 2..64) {
-        output = runRound(editedInputLength, inputList, output.index, output.skipValue)
-    }
-
-    val denseHashesList = output.outputList.chunked(16).map { computeXorOfList(it) }
-    return computeHexString(denseHashesList)
 }
 
 fun computeHexString(denseHashesList: List<Int>): String {
@@ -80,10 +74,6 @@ fun computeHexString(denseHashesList: List<Int>): String {
 
 fun computeXorOfList(chunk: List<Int>): Int {
     var xorResult = 0
-    for (i in 0 until 16) {
-        xorResult = xorResult.xor(chunk[i])
-    }
+    for (element in chunk) xorResult = xorResult.xor(element)
     return xorResult
 }
-
-class Day10(var outputList: MutableList<Int>, var index: Int = 0, var skipValue: Int = 0)
--- src/test/kotlin/aoc2017/day10/Day10KtTest.kt
+++ src/test/kotlin/aoc2017/day10/Day10KtTest.kt
@@ -21,8 +21,8 @@ internal class Day10KtTest {
 
     @Test
     fun testProcessList() {
-        assertEquals(12, processList(testInput, 5))
-        assertEquals(3770, processList(input, 256))
+        assertEquals(12, processListPart1(testInput, 5))
+        assertEquals(3770, processListPart1(input, 256))
     }
 
     @Test
@@ -31,16 +31,12 @@ internal class Day10KtTest {
     }
 
     @Test
-    fun test2() {
-        part2("")
-    }
-
-    @Test
     fun testProcessListPart2() {
         assertEquals("a2582a3a0e66e6e86e3812dcb672a272", processListPart2(convertListToASCII("")))
-//        assertEquals("33efeb34ea91902bb2f59c9920caa6cd", processListPart2(convertListToASCII("AoC 2017")))
-//        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", processListPart2(convertListToASCII("1,2,3")))
-//        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", processListPart2(asciiTestInput))
+        assertEquals("33efeb34ea91902bb2f59c9920caa6cd", processListPart2(convertListToASCII("AoC 2017")))
+        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", processListPart2(convertListToASCII("1,2,3")))
+        assertEquals("4a19451b02fb05416d73aea0ec8c00c0", processListPart2(asciiTestInput))
+        assertEquals("a9d0e68649d0174c8756a59ba21d4dc6", processListPart2(asciiInput))
     }
 
     @Test
