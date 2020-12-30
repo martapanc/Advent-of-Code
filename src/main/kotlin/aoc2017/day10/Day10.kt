42a7af72b6ae837e6eaf1ec6a233aef7939e5162
Marta Pancaldi <marta.panc@gmail.com>
2020-12-30 20:47:56 +0000

    AoC2017 D10P2: wip

--- src/main/kotlin/aoc2017/day10/Day10.kt
+++ src/main/kotlin/aoc2017/day10/Day10.kt
@@ -9,8 +9,15 @@ fun readInputToList(path: String): List<Int> {
 
 fun processList(inputLength: List<Int>, sizeOfList: Int): Long {
     var inputList = (0 until sizeOfList).toMutableList()
-    var index = 0
-    for ((skipValue, length) in inputLength.withIndex()) {
+    inputList = runRound(inputLength, inputList).outputList
+    return inputList[0].toLong() * inputList[1]
+}
+
+private fun runRound(lengths: List<Int>, numbers: List<Int>, initIndex: Int = 0, initSkipVal: Int = 0): Day10 {
+    var index = initIndex
+    var skipValue = initSkipVal
+    var inputList = numbers.toMutableList()
+    for ((_, length) in lengths.withIndex()) {
         if (index + length < inputList.size) {
             val selection = inputList.subList(index, index + length).reversed().toMutableList()
             for (value in selection) {
@@ -36,6 +43,47 @@ fun processList(inputLength: List<Int>, sizeOfList: Int): Long {
             inputList = inputList.toSet().toMutableList()
         }
         index = (index + (length + skipValue)) % inputList.size
+        skipValue++
     }
-    return inputList[0].toLong() * inputList[1]
+    return Day10(inputList, index, skipValue)
 }
+
+fun convertListToASCII(input: String, isPath: Boolean = false): List<Int> {
+    val asciiList = mutableListOf<Int>()
+    val inputString = if (isPath) readInputLineByLine(input)[0] else input
+    for (char in inputString) {
+        asciiList.add(char.toInt())
+    }
+    return asciiList
+}
+
+fun processListPart2(inputLength: List<Int>, sizeOfList: Int = 256): String {
+    val inputList = (0 until sizeOfList).toMutableList()
+    val editedInputLength = inputLength + listOf(17, 31, 73, 47, 23)
+    var output = runRound(editedInputLength, inputList)
+    for (i in 2..64) {
+        output = runRound(editedInputLength, inputList, output.index, output.skipValue)
+    }
+
+    val denseHashesList = output.outputList.chunked(16).map { computeXorOfList(it) }
+    return computeHexString(denseHashesList)
+}
+
+fun computeHexString(denseHashesList: List<Int>): String {
+    var hexString = ""
+    for (denseHash in denseHashesList) {
+        val toHexString = Integer.toHexString(denseHash)
+        hexString += if (toHexString.length == 1) "0$toHexString" else toHexString
+    }
+    return hexString
+}
+
+fun computeXorOfList(chunk: List<Int>): Int {
+    var xorResult = 0
+    for (i in 0 until 16) {
+        xorResult = xorResult.xor(chunk[i])
+    }
+    return xorResult
+}
+
+class Day10(var outputList: MutableList<Int>, var index: Int = 0, var skipValue: Int = 0)
--- src/test/kotlin/aoc2017/day10/Day10KtTest.kt
+++ src/test/kotlin/aoc2017/day10/Day10KtTest.kt
@@ -5,8 +5,13 @@ import kotlin.test.assertEquals
 
 internal class Day10KtTest {
 
-    private val testInput = readInputToList("src/main/kotlin/aoc2017/day10/test_input")
-    private val input = readInputToList("src/main/kotlin/aoc2017/day10/input")
+    private val testPath = "src/main/kotlin/aoc2017/day10/test_input"
+    private val path = "src/main/kotlin/aoc2017/day10/input"
+    private val testInput = readInputToList(testPath)
+    private val input = readInputToList(path)
+
+    private val asciiTestInput = convertListToASCII(testPath, true)
+    private val asciiInput = convertListToASCII(path, true)
 
     @Test
     fun readInputToList() {
@@ -17,6 +22,34 @@ internal class Day10KtTest {
     @Test
     fun testProcessList() {
         assertEquals(12, processList(testInput, 5))
-        assertEquals(37703770, processList(input, 256))
+        assertEquals(3770, processList(input, 256))
+    }
+
+    @Test
+    fun testConvertListToASCII() {
+        assertEquals(listOf(51, 44, 52, 44, 49, 44, 53), asciiTestInput)
+    }
+
+    @Test
+    fun test2() {
+        part2("")
+    }
+
+    @Test
+    fun testProcessListPart2() {
+        assertEquals("a2582a3a0e66e6e86e3812dcb672a272", processListPart2(convertListToASCII("")))
+//        assertEquals("33efeb34ea91902bb2f59c9920caa6cd", processListPart2(convertListToASCII("AoC 2017")))
+//        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", processListPart2(convertListToASCII("1,2,3")))
+//        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", processListPart2(asciiTestInput))
+    }
+
+    @Test
+    fun computeXorOfList() {
+        assertEquals(64, computeXorOfList(listOf(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22)))
+    }
+
+    @Test
+    fun computeHexString() {
+        assertEquals("4007ff", computeHexString(listOf(64, 7, 255)))
     }
 }
