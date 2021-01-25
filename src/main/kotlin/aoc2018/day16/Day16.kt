148d451785c58f9b00090ce84fef74d8484934a7
martapanc <marta.panc@gmail.com>
2021-01-25 18:00:09 +0000

    AoC2018 D16P2: complete

--- src/main/kotlin/aoc2018/day16/Day16.kt
+++ src/main/kotlin/aoc2018/day16/Day16.kt
@@ -1,6 +1,23 @@
 package aoc2018.day16
 
+import aoc2018.day16.Operation.addi
+import aoc2018.day16.Operation.addr
+import aoc2018.day16.Operation.bani
+import aoc2018.day16.Operation.banr
+import aoc2018.day16.Operation.bori
+import aoc2018.day16.Operation.borr
+import aoc2018.day16.Operation.eqir
+import aoc2018.day16.Operation.eqri
+import aoc2018.day16.Operation.eqrr
+import aoc2018.day16.Operation.gtir
+import aoc2018.day16.Operation.gtri
+import aoc2018.day16.Operation.gtrr
+import aoc2018.day16.Operation.muli
+import aoc2018.day16.Operation.mulr
+import aoc2018.day16.Operation.seti
+import aoc2018.day16.Operation.setr
 import util.readInputLineByLine
+import kotlin.reflect.jvm.javaMethod
 
 fun readInputPart1(path: String): List<Sample> {
     val sampleList = mutableListOf<Sample>()
@@ -14,15 +31,35 @@ fun readInputPart1(path: String): List<Sample> {
     return sampleList
 }
 
+fun readInputPart2(path: String): List<List<Int>> {
+    return readInputLineByLine(path).map { line -> line.split(" ").map { it.toInt() } }
+}
+
 fun countSamplesMatchingOpcodes(samples: List<Sample>): Int {
-    var matches3OrMoreOpcodesCount = 0
+    val opcodeMatchMap = mutableMapOf<Sample, List<String>>()
     for (sample in samples) {
-        val currentCount = Operation.methods.count { it(sample.before, sample.instruction) == sample.after }
-        if (currentCount >= 3) {
-            matches3OrMoreOpcodesCount++
-        }
+        opcodeMatchMap[sample] = Operation.methods
+            .filter { it(sample.before, sample.instruction) == sample.after }
+            .map {
+                it.javaMethod.toString()
+                    .replace("public final java.util.List aoc2018.day16.Operation.", "")
+                    .replace("(java.util.List,java.util.List)", "")
+            }
     }
-    return matches3OrMoreOpcodesCount
+    return opcodeMatchMap.values.count { it.size >= 3 }
+}
+
+fun runProgram(instructions: List<List<Int>>): Int {
+    val opcodeMap = mapOf(
+        0 to ::gtri, 1 to ::bani, 2 to ::eqrr, 3 to ::gtir, 4 to ::eqir, 5 to ::bori, 6 to ::seti, 7 to ::setr,
+        8 to ::addr, 9 to ::borr, 10 to ::muli, 11 to ::banr, 12 to ::addi, 13 to ::eqri, 14 to ::mulr, 15 to ::gtrr
+    )
+    var registers = listOf(0, 0, 0, 0)
+    for (instruction in instructions) {
+        val method = opcodeMap[instruction[0]]
+        registers = method!!(registers, instruction)
+    }
+    return registers[0]
 }
 
 private object Operation {
--- src/test/kotlin/aoc2018/day16/Day16KtTest.kt
+++ src/test/kotlin/aoc2018/day16/Day16KtTest.kt
@@ -7,14 +7,21 @@ import org.junit.jupiter.api.Assertions.*
 internal class Day16KtTest {
 
     private val input1 = readInputPart1("src/main/kotlin/aoc2018/day16/input_1")
+    private val input2 = readInputPart2("src/main/kotlin/aoc2018/day16/input_2")
 
     @Test
-    fun testReadInputPart1() {
+    fun testReadInput() {
         assertEquals(782, input1.size)
+        assertEquals(946, input2.size)
     }
 
     @Test
     fun testCountSamplesMatchingOpcodes() {
         assertEquals(542, countSamplesMatchingOpcodes(input1))
+    }
+
+    @Test
+    fun testRunProgram() {
+        assertEquals(575, runProgram(input2))
     }
 }
