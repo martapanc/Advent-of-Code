db4fbe887de4f1d6337e5db9b7413a6942112fd1
martapanc <marta.panc@gmail.com>
2021-01-29 00:35:50 +0000

    AoC2016 D01P1: complete

--- src/main/kotlin/aoc2016/day01/Day1.kt
+++ src/main/kotlin/aoc2016/day01/Day1.kt
@@ -1,4 +1,37 @@
 package aoc2016.day01
 
-class Day1 {
+import aoc2020.day20.Coord
+import kotlin.math.abs
+
+fun computeDistance(input: String): Int {
+    var currentPos = Coord(0, 0)
+    var facing = Facing.N
+    val movements = input.split(", ")
+        .map { Movement(Rotation.valueOf(it[0].toString()), it.replace(Regex("[R|L]"), "").toInt()) }
+
+    for (mov in movements) {
+        facing = facing.turn(mov.rotationDir)
+        val delta = deltas[Facing.values().indexOf(facing)]
+        currentPos = Coord(currentPos.x + mov.distance * delta.x, currentPos.y + mov.distance * delta.y)
+    }
+    return currentPos.manhattan()
+}
+
+data class Movement(val rotationDir: Rotation, var distance: Int)
+
+enum class Rotation { R, L }
+
+enum class Facing { N, E, S, W }
+
+val deltas = listOf(Coord(0, -1), Coord(1, 0), Coord(0, 1), Coord(-1, 0))
+
+fun Facing.turn(rotation: Rotation): Facing {
+    val currentIndex = Facing.values().indexOf(this)
+    var newIndex = (if (rotation == Rotation.R) currentIndex + 1 else currentIndex - 1) % 4
+    if (newIndex == -1) newIndex = 3
+    return Facing.values()[newIndex]
+}
+
+fun Coord.manhattan(): Int {
+    return abs(0 - this.x) + abs(0 - this.y)
 }
---
+++ src/test/kotlin/aoc2016/day01/Day1KtTest.kt
@@ -0,0 +1,25 @@
+package aoc2016.day01
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import util.readInputLineByLine
+
+internal class Day1KtTest {
+
+    private val input = readInputLineByLine("src/main/kotlin/aoc2016/day01/input")[0]
+
+    @Test
+    fun testComputeDistance() {
+        assertEquals(5, computeDistance("R2, L3"))
+        assertEquals(2, computeDistance("R2, R2, R2"))
+        assertEquals(12, computeDistance("R5, L5, R5, R3"))
+        assertEquals(161, computeDistance(input))
+    }
+
+    @Test
+    fun testComputeDistancePart2() {
+//        assertEquals(4, computeDistance("R8, R4, R4, R8"))
+//        assertEquals(161, computeDistance(input))
+    }
+}
