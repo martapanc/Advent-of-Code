ace0a9079ec9e2185e327870d4e4c4ebf75e009a
martapanc <marta.panc@gmail.com>
2021-12-03 09:02:26 +0000

    D03P2: refactor

--- src/main/kotlin/aoc2021/day03/Day3.kt
+++ src/main/kotlin/aoc2021/day03/Day3.kt
@@ -7,12 +7,7 @@ fun getPowerConsumption(inputList: List<String>): Long {
     var epsilonRate = ""
 
     for (i in 0 until inputList[0].length) {
-        val positionList = mutableListOf<Int>()
-        inputList.forEach { number: String ->
-            positionList.add(number[i].toString().toInt())
-        }
-        val zeroCount = positionList.count { x -> x == 0 }
-        val oneCount = positionList.count { x -> x == 1 }
+        val (zeroCount, oneCount) = getBitCounts(inputList, i)
 
         if (zeroCount > oneCount) {
             gammaRate += 0
@@ -27,43 +22,38 @@ fun getPowerConsumption(inputList: List<String>): Long {
 }
 
 fun getLifeSupportRating(inputList: List<String>): Long {
-
     val oxygenNumberList = inputList.toMutableList()
     val co2NumberList = inputList.toMutableList()
 
     for (i in 0 until inputList[0].length) {
-        val oxygenPositionList = mutableListOf<Int>()
-
-        if (oxygenNumberList.size > 1) {
-            oxygenNumberList.forEach { number: String ->
-                oxygenPositionList.add(number[i].toString().toInt())
-            }
-            val zeroCount = oxygenPositionList.count { x -> x == 0 }
-            val oneCount = oxygenPositionList.count { x -> x == 1 }
-
-            if (zeroCount > oneCount) {
-                oxygenNumberList.removeAll { number -> number[i] != '0' }
-            } else {
-                oxygenNumberList.removeAll { number -> number[i] != '1' }
-            }
-        }
-
-        val co2PositionList = mutableListOf<Int>()
-
-        if (co2NumberList.size > 1) {
-            co2NumberList.forEach { number: String ->
-                co2PositionList.add(number[i].toString().toInt())
-            }
-            val zeroCount = co2PositionList.count { x -> x == 0 }
-            val oneCount = co2PositionList.count { x -> x == 1 }
-
-            if (zeroCount > oneCount) {
-                co2NumberList.removeAll { number -> number[i] != '1' }
-            } else {
-                co2NumberList.removeAll { number -> number[i] != '0' }
-            }
-        }
+        updateValueList(oxygenNumberList, i)
+        updateValueList(co2NumberList, i, '1', '0')
     }
 
     return binaryToDecimal(oxygenNumberList[0]) * binaryToDecimal(co2NumberList[0])
+}
+
+private fun updateValueList(
+    valueList: MutableList<String>,
+    bitPosition: Int,
+    bitToKeep: Char = '0',
+    bitToDiscard: Char = '1'
+) {
+    if (valueList.size > 1) {
+        val (zeroCount, oneCount) = getBitCounts(valueList, bitPosition)
+
+        if (zeroCount > oneCount) {
+            valueList.removeAll { number -> number[bitPosition] != bitToKeep }
+        } else {
+            valueList.removeAll { number -> number[bitPosition] != bitToDiscard }
+        }
+    }
+}
+
+private fun getBitCounts(inputList: List<String>, i: Int): Pair<Int, Int> {
+    val positionList = mutableListOf<Int>()
+    inputList.forEach { number: String ->
+        positionList.add(number[i].toString().toInt())
+    }
+    return Pair(positionList.count { x -> x == 0 }, positionList.count { x -> x == 1 })
 }
