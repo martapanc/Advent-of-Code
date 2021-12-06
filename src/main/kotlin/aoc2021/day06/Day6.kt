d4609d0baf87529be3ccf0a6d065a2eb40bd6b22
martapanc <marta.panc@gmail.com>
2021-12-06 15:08:20 +0000

    D06P2: complete

--- src/main/kotlin/aoc2021/day06/Day6.kt
+++ src/main/kotlin/aoc2021/day06/Day6.kt
@@ -17,6 +17,32 @@ fun getLanternfishGrowth(lanternfishList: List<Int>, days: Int = 1): Int {
     return mutableLanternfishList.size
 }
 
-//fun getLanternfishGrowthPart2(lanternfishList: List<Int>, days: Int = 1): Int {
-//
-//}
+fun getLanternfishGrowthPart2(lanternfishList: List<Int>, days: Int = 1): Long {
+    var lanternfishMap = mutableMapOf<Int, Long>()
+    (0..8).forEach{ lanternfishMap[it] = 0}
+
+    for (lanternfish: Int in lanternfishList) {
+        lanternfishMap[lanternfish] = lanternfishMap[lanternfish]!! + 1
+    }
+
+    for (day in 0 until days) {
+        val newFish = mutableMapOf<Int, Long>()
+        (0..8).forEach{ newFish[it] = 0}
+
+        for (fishTimerToQuantity: Map.Entry<Int, Long> in lanternfishMap.entries) {
+            val fishTimer = fishTimerToQuantity.key - 1
+            val quantity = fishTimerToQuantity.value
+
+            if (fishTimerToQuantity.key == 0) {
+                newFish[6] =  newFish[6]!! + quantity
+                newFish[8] = newFish[8]!! + quantity
+            } else {
+                newFish[fishTimer] = newFish[fishTimer]!! + quantity
+            }
+        }
+
+        lanternfishMap = newFish
+    }
+
+    return lanternfishMap.values.sum()
+}
--- src/test/kotlin/aoc2021/day06/Day6KtTest.kt
+++ src/test/kotlin/aoc2021/day06/Day6KtTest.kt
@@ -25,9 +25,29 @@ internal class Day6KtTest {
         assertEquals(28543, getLanternfishGrowth(input0, 98))
         assertEquals(30822, getLanternfishGrowth(input0, 99))
         assertEquals(33893, getLanternfishGrowth(input0, 100))
-//        assertEquals(26984457539, getLanternfishGrowth(input0, 256))
 
         assertEquals(300, getLanternfishGrowth(input, 0))
         assertEquals(345793, getLanternfishGrowth(input, 80))
+    }
+
+    @Test
+    fun getLanternfishGrowthPart2() {
+        assertEquals(5, getLanternfishGrowthPart2(input0, 0))
+        assertEquals(5, getLanternfishGrowthPart2(input0, 1))
+        assertEquals(6, getLanternfishGrowthPart2(input0, 2))
+        assertEquals(7, getLanternfishGrowthPart2(input0, 3))
+        assertEquals(9, getLanternfishGrowthPart2(input0, 4))
+        assertEquals(19, getLanternfishGrowthPart2(input0, 13))
+        assertEquals(26, getLanternfishGrowthPart2(input0, 18))
+        assertEquals(5934, getLanternfishGrowthPart2(input0, 80))
+        assertEquals(25724, getLanternfishGrowthPart2(input0, 97))
+        assertEquals(28543, getLanternfishGrowthPart2(input0, 98))
+        assertEquals(30822, getLanternfishGrowthPart2(input0, 99))
+        assertEquals(33893, getLanternfishGrowthPart2(input0, 100))
+        assertEquals(26984457539, getLanternfishGrowthPart2(input0, 256))
+
+        assertEquals(300, getLanternfishGrowthPart2(input, 0))
+        assertEquals(345793, getLanternfishGrowthPart2(input, 80))
+        assertEquals(1572643095893, getLanternfishGrowthPart2(input, 256))
     }
 }
