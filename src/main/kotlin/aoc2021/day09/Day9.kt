48873ac1fd8707daddc920fe5b5409a2966db47e
martapanc <marta.panc@gmail.com>
2021-12-09 13:57:18 +0000

    D09P1: refactor

--- src/main/kotlin/aoc2021/day09/Day9.kt
+++ src/main/kotlin/aoc2021/day09/Day9.kt
@@ -6,11 +6,11 @@ import util.readInputLineByLine
 fun readInputToMap(path: String): Map<Coord, Int> {
   val map = mutableMapOf<Coord, Int>()
   var x = 0
-  for ((y, line) in readInputLineByLine(path).withIndex()) {
-    for (char in line) {
-      map[Coord(x, y)] = Character.getNumericValue(char)
-      x++
-    }
+  readInputLineByLine(path).withIndex().forEach { (y, line) ->
+      for (char in line) {
+        map[Coord(x, y)] = Character.getNumericValue(char)
+        x++
+      }
     x = 0
   }
   return map
@@ -18,18 +18,15 @@ fun readInputToMap(path: String): Map<Coord, Int> {
 
 fun findSumOfRiskLevels(inputMap: Map<Coord, Int>): Int {
   var riskLevelSum = 0
-  inputMap.entries.forEach { heightPoint : Map.Entry<Coord, Int> ->
-    val neighbors = heightPoint.key.neighbors()
-    val heightValue = heightPoint.value
-
+  inputMap.entries.forEach { heightPoint: Map.Entry<Coord, Int> ->
     val nonNullNeighbors = mutableListOf<Coord>()
-    neighbors.forEach { n ->
+    heightPoint.key.neighbors().forEach { n ->
       if (inputMap[n] != null) {
         nonNullNeighbors.add(n)
       }
     }
-    if (nonNullNeighbors.all { inputMap[it]!! > heightValue }) {
-      riskLevelSum += heightValue + 1
+    if (nonNullNeighbors.all { inputMap[it]!! > heightPoint.value }) {
+      riskLevelSum += heightPoint.value + 1
     }
   }
   return riskLevelSum
