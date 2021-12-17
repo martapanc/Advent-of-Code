d34d361d4bc666dc7ef596eabfd4940aea4d203f
martapanc <marta.panc@gmail.com>
2021-12-17 18:55:50 +0100

    D13P2: wip

--- src/main/kotlin/aoc2021/day13/Day13.kt
+++ src/main/kotlin/aoc2021/day13/Day13.kt
@@ -4,20 +4,27 @@ import aoc2020.day20.Coord
 import util.readInputLineByLine
 import kotlin.math.abs
 
-fun readInputToCoordList(input: String): List<Coord> {
+fun readInputToCoordAndFoldList(input: String): Instructions {
   val coordList = mutableListOf<Coord>()
+  val foldList = mutableListOf<Fold>()
   for (line in readInputLineByLine(input)) {
     if (line.isEmpty()) {
-      break
+      continue
+    } else {
+      if (line.startsWith("fold")) {
+        val split = line.replace("fold along ", "").split("=")
+        foldList.add(Fold(Direction.valueOf(split[0].toUpperCase()), split[1].toInt()))
+      } else {
+        val split = line.split(",").map { it.toInt() }
+        coordList.add(Coord(split[0], split[1]))
+      }
     }
-    val split = line.split(",").map { it.toInt() }
-    coordList.add(Coord(split[0], split[1]))
   }
 
-  return coordList
+  return Instructions(coordList, foldList)
 }
 
-fun foldOrigamiAndCompute(coordList: List<Coord>, foldAlongX: Int? = null, foldAlongY: Int? = null): Int {
+fun foldAndCompute(coordList: List<Coord>, foldAlongX: Int? = null, foldAlongY: Int? = null): Set<Coord> {
   val foldedList = mutableSetOf<Coord>()
 
   if (foldAlongY != null) {
@@ -32,5 +39,36 @@ fun foldOrigamiAndCompute(coordList: List<Coord>, foldAlongX: Int? = null, foldA
     }
   }
 
-  return foldedList.size
+  return foldedList
+}
+
+fun foldOrigami(coordList: List<Coord>, foldList: List<Fold>) {
+  var mutableCoordList = coordList.toMutableList()
+//  printCoords(mutableCoordList)
+  for (fold in foldList) {
+    val foldedCoords =
+      if (fold.direction == Direction.X) {
+        foldAndCompute(mutableCoordList, foldAlongX = fold.foldValue)
+      } else {
+        foldAndCompute(mutableCoordList, foldAlongY = fold.foldValue)
+      }
+
+    mutableCoordList = foldedCoords.toMutableList()
+  }
+  printCoords(mutableCoordList)
+}
+
+class Fold(val direction: Direction, val foldValue: Int)
+
+enum class Direction { X, Y }
+
+class Instructions(val coordinates: List<Coord>, val folds: List<Fold>)
+
+fun printCoords(coords: List<Coord>) {
+  for (y in 0..coords.maxOf { it.y }) {
+    for (x in 0..coords.maxOf { it.x })
+      if (coords.contains(Coord(x, y))) print("#") else print(".")
+    println()
+  }
+  println()
 }
--- src/test/kotlin/aoc2021/day13/Day13KtTest.kt
+++ src/test/kotlin/aoc2021/day13/Day13KtTest.kt
@@ -6,12 +6,18 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class Day13KtTest {
 
-  val input0 = readInputToCoordList("src/main/kotlin/aoc2021/day13/assets/input0")
-  val input = readInputToCoordList("src/main/kotlin/aoc2021/day13/assets/input")
+  val input0 = readInputToCoordAndFoldList("src/main/kotlin/aoc2021/day13/assets/input0")
+  val input = readInputToCoordAndFoldList("src/main/kotlin/aoc2021/day13/assets/input")
 
   @Test
-    fun foldOrigamiAndCompute() {
-      assertEquals(17, foldOrigamiAndCompute(input0, foldAlongY = 7))
-      assertEquals(704, foldOrigamiAndCompute(input, foldAlongX=655))
-    }
+  fun foldOrigamiAndCompute() {
+    assertEquals(17, foldAndCompute(input0.coordinates, foldAlongY = 7).size)
+    assertEquals(704, foldAndCompute(input.coordinates, foldAlongX = 655).size)
+  }
+
+  @Test
+  fun foldOrigamiAndComputePart2() {
+    foldOrigami(input0.coordinates, input0.folds)
+    foldOrigami(input.coordinates, input.folds)
+  }
 }
