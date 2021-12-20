c96a9fad2f75d300b24dc65ede8c1f8732fd502d
martapanc <marta.panc@gmail.com>
2021-12-20 21:46:42 +0100

    D14P2: complete

--- src/main/kotlin/aoc2021/day14/Day14.kt
+++ src/main/kotlin/aoc2021/day14/Day14.kt
@@ -11,7 +11,17 @@ fun readInputToInsertionRules(inputString: String): Map<String, String> {
   return insertionRules
 }
 
-fun computePolimerisation(insertionRules: Map<String, String>, initialPolymer: String, iterations: Int = 1): String {
+fun readInputToInsertionRules2(inputString: String): Map<String, Pair<String, String>> {
+  val insertionRules = mutableMapOf<String, Pair<String, String>>()
+  for (line in readInputLineByLine(inputString)) {
+    val split = line.split(" -> ")
+    // E.g. CH -> B becomes CH -> CB, BH for easier replacement
+    insertionRules[split[0]] = Pair(split[0][0] + split[1], split[1] + split[0][1])
+  }
+  return insertionRules
+}
+
+fun computePolymerisation(insertionRules: Map<String, String>, initialPolymer: String, iterations: Int = 1): String {
   var newPolymer = initialPolymer
   (1..iterations).forEach { _ ->
     var firstTwoChars = newPolymer.subSequence(0, 2)
@@ -30,12 +40,67 @@ fun computePolimerisation(insertionRules: Map<String, String>, initialPolymer: S
   return newPolymer
 }
 
-fun computeChecksum(insertionRules: Map<String, String>, initialPolymer: String, iterations: Int): Int {
-  val finalPolymer = computePolimerisation(insertionRules, initialPolymer, iterations)
+fun computeChecksum(insertionRules: Map<String, String>, initialPolymer: String): Int {
+  val finalPolymer = computePolymerisation(insertionRules, initialPolymer, 10)
   val frequencies = mutableMapOf<Char, Int>()
-  for (uniqueChar : Char in finalPolymer.toCharArray().toSet()) {
+  for (uniqueChar: Char in finalPolymer.toCharArray().toSet()) {
     frequencies[uniqueChar] = finalPolymer.count { it == uniqueChar }
   }
   return frequencies.maxOf { it.value } - frequencies.minOf { it.value }
 }
 
+fun computePolymerisationOptimised(
+  insertionRules: Map<String, Pair<String, String>>,
+  initialPolymer: String,
+  iterations: Int = 1
+): Map<String, Long> {
+  var polymer = mutableMapOf<String, Long>()
+  initialPolymer.windowed(2, 1).forEach { pair -> polymer[pair] = 1 }
+  (1..iterations).forEach { _ ->
+    val newPolymer = mutableMapOf<String, Long>()
+    for (pair in polymer.entries) {
+      val newPairs = insertionRules[pair.key]!!
+
+      if (newPolymer.containsKey(newPairs.first)) {
+        newPolymer[newPairs.first] = newPolymer[newPairs.first]!! + pair.value
+      } else {
+
+//        if (polymer.containsKey(newPairs.first)) {
+//          newPolymer[newPairs.first] = polymer[newPairs.first]!!
+//        } else {
+          newPolymer[newPairs.first] = pair.value
+//        }
+      }
+
+      if (newPolymer.containsKey(newPairs.second)) {
+        newPolymer[newPairs.second] = newPolymer[newPairs.second]!! + pair.value
+      } else {
+
+//        if (polymer.containsKey(newPairs.second)) {
+//          newPolymer[newPairs.second] = polymer[newPairs.second]!!
+//        } else {
+          newPolymer[newPairs.second] = pair.value
+//        }
+      }
+    }
+
+    polymer = newPolymer
+  }
+
+  return polymer
+}
+
+fun computeChecksumPart2(insertionRules: Map<String, Pair<String, String>>, initialPolymer: String, iterations: Int): Long {
+  val finalPolymer = computePolymerisationOptimised(insertionRules, initialPolymer, iterations)
+  val frequencies = mutableMapOf<Char, Long>()
+
+  for (entry in finalPolymer.entries) {
+    if (frequencies.containsKey(entry.key[0])) {
+      frequencies[entry.key[0]] = frequencies[entry.key[0]]!! + entry.value
+    } else {
+      frequencies[entry.key[0]] = entry.value
+    }
+  }
+
+  return frequencies.maxOf { it.value } - frequencies.minOf { it.value }
+}
--- src/test/kotlin/aoc2021/day14/Day14KtTest.kt
+++ src/test/kotlin/aoc2021/day14/Day14KtTest.kt
@@ -6,23 +6,33 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class Day14KtTest {
 
-  val input0 = readInputToInsertionRules("src/main/kotlin/aoc2021/day14/assets/input0")
-  val polymer0 = "NNCB"
+  private val input0 = readInputToInsertionRules("src/main/kotlin/aoc2021/day14/assets/input0")
+  private val input0_2 = readInputToInsertionRules2("src/main/kotlin/aoc2021/day14/assets/input0")
+  private val polymer0 = "NNCB"
 
-  val input = readInputToInsertionRules("src/main/kotlin/aoc2021/day14/assets/input")
-  val polymer = "OFSVVSFOCBNONHKFHNPK"
+  private val input = readInputToInsertionRules("src/main/kotlin/aoc2021/day14/assets/input")
+  private val input_2 = readInputToInsertionRules2("src/main/kotlin/aoc2021/day14/assets/input")
+  private val polymer = "OFSVVSFOCBNONHKFHNPK"
 
   @Test
   fun computePolimerisation() {
-    assertEquals("NCNBCHB", computePolimerisation(input0, polymer0, 1))
-    assertEquals("NBCCNBBBCBHCB", computePolimerisation(input0, polymer0, 2))
-    assertEquals("NBBBCNCCNBBNBNBBCHBHHBCHB", computePolimerisation(input0, polymer0, 3))
-    assertEquals("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", computePolimerisation(input0, polymer0, 4))
+    assertEquals("NCNBCHB", computePolymerisation(input0, polymer0, 1))
+    assertEquals("NBCCNBBBCBHCB", computePolymerisation(input0, polymer0, 2))
+    assertEquals("NBBBCNCCNBBNBNBBCHBHHBCHB", computePolymerisation(input0, polymer0, 3))
+    assertEquals("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", computePolymerisation(input0, polymer0, 4))
   }
 
   @Test
   fun computeChecksum() {
-    assertEquals(1588, computeChecksum(input0, polymer0, 10))
-    assertEquals(3284, computeChecksum(input, polymer, 10))
+    assertEquals(1588, computeChecksum(input0, polymer0))
+    assertEquals(3284, computeChecksum(input, polymer))
+  }
+
+  @Test
+  fun testComputeChecksumPart2() {
+    assertEquals(18, computeChecksumPart2(input0_2, polymer0, 4) + 1)
+    assertEquals(1588, computeChecksumPart2(input0_2, polymer0, 10) + 1)
+    assertEquals(2188189693529, computeChecksumPart2(input0_2, polymer0, 40) + 1)
+    assertEquals(4302675529690, computeChecksumPart2(input_2, polymer, 40))
   }
 }
