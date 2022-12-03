6b0a163ffb67e756a5d2213a0dfd5341865fb610
martapanc <marta.panc@gmail.com>
2022-12-03 11:29:39 +0100

    [Day3] Part 2

--- src/main/kotlin/aoc2022/day03/Day3.kt
+++ src/main/kotlin/aoc2022/day03/Day3.kt
@@ -1,19 +1,36 @@
 package aoc2022.day03
 
-fun computePrioritySum(list: List<String>): Int {
+fun computePrioritySumPart1(list: List<String>): Int {
+    val itemValueMap = getItemValueMap()
+    var prioritySum = 0
+    list.forEach { line ->
+        val compartment1 = line.subSequence(0, line.length / 2).toSet()
+        val compartment2 = line.subSequence(line.length / 2, line.length).toSet()
+        val commonItem: Char = compartment1.intersect(compartment2).first()
+        prioritySum += itemValueMap[commonItem]!!
+    }
+    return prioritySum
+}
+
+fun computePrioritySumPart2(list: List<String>): Int {
+    val itemValueMap = getItemValueMap()
+    var prioritySum = 0
+    for (i in list.indices step 3) {
+        val rucksack1 = list[i].toSet()
+        val rucksack2 = list[i + 1].toSet()
+        val rucksack3 = list[i + 2].toSet()
+        val commonItem: Char = rucksack1.intersect(rucksack2).intersect(rucksack3).first()
+        prioritySum += itemValueMap[commonItem]!!
+    }
+    return prioritySum
+}
+
+private fun getItemValueMap(): MutableMap<Char, Int> {
     val itemValueMap = mutableMapOf<Char, Int>()
     var value = 1
     var char = 'a'
     while (char <= 'z') itemValueMap[char++] = value++
     char = 'A'
     while (char <= 'Z') itemValueMap[char++] = value++
-
-    var prioritySum = 0
-    list.forEach { line ->
-        val compartment1 = line.subSequence(0, line.length / 2).toSet()
-        val compartment2 = line.subSequence(line.length / 2, line.length).toSet()
-        val commonItems: Char = compartment1.intersect(compartment2).first()
-        prioritySum += itemValueMap[commonItems]!!
-    }
-    return prioritySum
+    return itemValueMap
 }
--- src/test/kotlin/aoc2022/day03/Day3KtTest.kt
+++ src/test/kotlin/aoc2022/day03/Day3KtTest.kt
@@ -1,8 +1,7 @@
 package aoc2022.day03
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import util.readInputLineByLine
 
 internal class Day3KtTest {
@@ -11,8 +10,14 @@ internal class Day3KtTest {
     private val testInput = readInputLineByLine("src/main/kotlin/aoc2022/day03/assets/input")
 
     @Test
-    fun computePrioritySum() {
-        assertEquals(157, computePrioritySum(testInput0))
-        assertEquals(8085, computePrioritySum(testInput))
+    fun computePrioritySumPart1() {
+        assertEquals(157, computePrioritySumPart1(testInput0))
+        assertEquals(8085, computePrioritySumPart1(testInput))
+    }
+
+    @Test
+    fun computePrioritySumPart2() {
+        assertEquals(70, computePrioritySumPart2(testInput0))
+        assertEquals(2515, computePrioritySumPart2(testInput))
     }
 }
