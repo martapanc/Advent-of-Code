8552a3967db4b3c97821aa608e0f6d64b7f54543
martapanc <marta.panc@gmail.com>
2022-12-04 12:04:53 +0100

    [2021][16] Part 1 & 2

--- src/test/kotlin/aoc2021/day16/Day16KtTest.kt
+++ src/test/kotlin/aoc2021/day16/Day16KtTest.kt
@@ -6,11 +6,6 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class Day16KtTest {
 
-  private val testInput0 = "D2FE28"
-  private val testInput1 = "38006F45291200"
-  private val testInput2 = "EE00D40C823060"
-  private val testInput3 = "8A004A801A8002F478"
-
   private val testInput = "src/main/kotlin/aoc2021/day16/assets/input"
 
   @Test
