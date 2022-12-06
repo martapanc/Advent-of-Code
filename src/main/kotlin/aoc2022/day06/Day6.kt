3e58d4580d5e0d626981e1f4b6731366187e2f25
martapanc <marta.panc@gmail.com>
2022-12-06 09:16:19 +0100

    [Day 6] Refactor

--- src/main/kotlin/aoc2022/day06/Day6.kt
+++ src/main/kotlin/aoc2022/day06/Day6.kt
@@ -1,20 +1,18 @@
 package aoc2022.day06
 
 fun part1(list: String): Int {
-    for (i : Int in 0 .. list.length - 4) {
-        val packet = list.subSequence(i, i + 4).toString()
-        if (packet.allUnique()) {
-            return i + 4
-        }
-    }
-    return 0
+    return solve(list, 4)
 }
 
 fun part2(list: String): Int {
-    for (i : Int in 0 .. list.length - 14) {
-        val packet = list.subSequence(i, i + 14).toString()
+    return solve(list, 14)
+}
+
+private fun solve(list: String, packetSize: Int): Int {
+    for (i: Int in 0..list.length - packetSize) {
+        val packet = list.subSequence(i, i + packetSize).toString()
         if (packet.allUnique()) {
-            return i + 14
+            return i + packetSize
         }
     }
     return 0
