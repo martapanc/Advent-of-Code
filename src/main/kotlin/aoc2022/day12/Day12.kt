3e1ed6aaaef5242ea097de73e97ffcdac2010938
martapanc <marta.panc@gmail.com>
2022-12-12 14:36:46 +0100

    [Day 12] Part 1

--- src/main/kotlin/aoc2022/day12/Day12.kt
+++ src/main/kotlin/aoc2022/day12/Day12.kt
@@ -1,12 +1,56 @@
 package aoc2022.day12
 
 import util.Coord
+import util.deltas
 
 
 fun part1(grid: Map<Coord, Char>): Int {
-    return 0
+    val startCoord = grid.entries.first { it.value == 'S' }.key
+    val endCoord = grid.entries.first { it.value == 'E' }.key
+    return bfs(grid, startCoord, endCoord)
 }
 
 fun part2(grid: Map<Coord, Char>): Int {
     return 0
 }
+
+fun bfs(grid: Map<Coord, Char>, source: Coord, destination: Coord): Int {
+    val queue = ArrayDeque<Distance>()
+    queue.add(Distance(0, source))
+    val visited = mutableSetOf<Coord>()
+
+    while (queue.isNotEmpty()) {
+        val distance = queue.removeFirst()
+        if (distance.coord == destination) {
+            return distance.fromStart
+        }
+        if (distance.coord !in visited) {
+            visited.add(distance.coord)
+            for (neighbor in getValidNeighbors(grid, distance.coord)) {
+                if (neighbor in visited)
+                    continue
+                queue.addLast(Distance(distance.fromStart + 1, neighbor))
+            }
+        }
+    }
+    return -1
+}
+
+fun getValidNeighbors(grid: Map<Coord, Char>, currentCoord: Coord): Set<Coord> {
+    val validNeighbors = mutableSetOf<Coord>()
+    val neighbors = deltas.map { Coord(currentCoord.x + it.x, currentCoord.y + it.y) }
+    neighbors.forEach { neighbor ->
+        val neighborCell = grid[neighbor]
+        val currentCell = grid[currentCoord]!!
+        if (neighborCell != null)
+            if ((currentCell == 'S' && neighborCell == 'a')
+                || (currentCell == 'z' && neighborCell == 'E')
+                || (currentCell + 1 == neighborCell || (neighborCell != 'E' && currentCell >= neighborCell))
+            ) {
+                validNeighbors.add(neighbor)
+            }
+    }
+    return validNeighbors
+}
+
+data class Distance(val fromStart: Int, val coord: Coord)
--- src/test/kotlin/aoc2022/day12/Day12KtTest.kt
+++ src/test/kotlin/aoc2022/day12/Day12KtTest.kt
@@ -2,7 +2,7 @@ package aoc2022.day12
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import util.readInputLineByLine
+import util.Coord
 import util.readInputToMap
 
 internal class Day12KtTest {
@@ -12,13 +12,21 @@ internal class Day12KtTest {
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput0))
-        assertEquals(8085, part1(testInput))
+        assertEquals(31, part1(testInput0))
+        assertEquals(447, part1(testInput))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
+        assertEquals(29, part2(testInput0))
         assertEquals(2515, part2(testInput))
+    }
+
+    @Test
+    fun testGetValidNeighbors() {
+        assertEquals(setOf(Coord(1,0), Coord(0, 1)), getValidNeighbors(testInput0, Coord(0, 0)))
+        assertEquals(setOf(Coord(0,0), Coord(2,0), Coord(1, 1)), getValidNeighbors(testInput0, Coord(1, 0)))
+        assertEquals(setOf(Coord(1,0), Coord(2, 1)), getValidNeighbors(testInput0, Coord(2, 0)))
+        assertEquals(setOf(Coord(5, 2), Coord(4, 1), Coord(3, 2), Coord(4, 3)), getValidNeighbors(testInput0, Coord(4, 2)))
     }
 }
