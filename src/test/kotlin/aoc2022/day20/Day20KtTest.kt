81fd109cf60a2a42a466072109edc6b06a52dad6
martapanc <marta.panc@gmail.com>
2022-12-20 16:44:02 +0100

    [Day 20] Part 2 - wip

--- src/test/kotlin/aoc2022/day20/Day20KtTest.kt
+++ src/test/kotlin/aoc2022/day20/Day20KtTest.kt
@@ -6,19 +6,18 @@ import org.junit.jupiter.api.Test
 internal class Day20KtTest {
 
     private val testInput0 = readInputToNumbers("src/main/kotlin/aoc2022/day20/assets/input0")
-    private val testInput1 = readInputToNumbers("src/main/kotlin/aoc2022/day20/assets/input1")
     private val testInput = readInputToNumbers("src/main/kotlin/aoc2022/day20/assets/input")
 
     @Test
     fun testPart1() {
-        assertEquals(3, part1(testInput0))
-//        assertEquals(0, part1(testInput1))
-        assertEquals(8085, part1(testInput))
+        assertEquals(3, Day20(testInput0).part1())
+        assertEquals(9866, Day20(testInput).part1())
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+        assertEquals(1623178306, Day20(testInput0).part2())
+        assertEquals(12374299815791, Day20(testInput).part2())
     }
+
 }
