c30fa86346906bfcf83686152800f902eb49eae6
martapanc <marta.panc@gmail.com>
2022-12-20 16:46:02 +0100

    [Day 20] Part 2 - wip

--- src/main/kotlin/aoc2022/day20/Day20.kt
+++ src/main/kotlin/aoc2022/day20/Day20.kt
@@ -2,35 +2,47 @@ package aoc2022.day20
 
 import util.readInputLineByLine
 
-
 fun readInputToNumbers(path: String): List<Int> = readInputLineByLine(path).map { it.toInt() }
 
-fun part1(numbers: List<Int>): Int {
-    val size = numbers.size
-    val sortedNumbers = numbers.toMutableList()
-    numbers.forEach { num ->
-        val currIndex = sortedNumbers.indexOf(num)
-        val targetIndex = (sortedNumbers[currIndex] + currIndex) % size
-        if (num > 0) {
-            var newIndex = targetIndex % size
-            newIndex = if (newIndex > currIndex) newIndex - 1 else newIndex
-            val numToMove = sortedNumbers.removeAt(currIndex)
-            sortedNumbers.add(newIndex, numToMove)
-        } else {
-            var newIndex = targetIndex % size
-            newIndex = if (newIndex > currIndex) newIndex + 1 else newIndex
-            val numToMove = sortedNumbers.removeAt(currIndex)
-            sortedNumbers.add(newIndex, numToMove)
-        }
+class Day20(lines: List<Int>) {
+    private val boxes = lines.map { Box(it) }
 
+    fun part1(): Int {
+        val array = boxes.toTypedArray()
+        array.mix()
+        val base = array.indexOfFirst { it.value == 0 }
+        check(base >= 0)
+        return array[(base + 1000) % array.size].value +
+                array[(base + 2000) % array.size].value +
+                array[(base + 3000) % array.size].value
     }
-    val indexOfZero = sortedNumbers.indexOf(0)
-    val pos1000 = (indexOfZero + 1000) % size
-    val pos2000 = (indexOfZero + 2000) % size
-    val pos3000 = (indexOfZero + 3000) % size
-    return sortedNumbers[pos1000] + sortedNumbers[pos2000] + sortedNumbers[pos3000]
-}
 
-fun part2(numbers: List<Int>): Int {
-    return 0
+    fun part2(): Long {
+        val array = boxes.toTypedArray()
+        repeat(10) { array.mix(MULTIPLIER) }
+        val base = array.indexOfFirst { it.value == 0 }
+        check(base >= 0)
+        return MULTIPLIER * (
+                array[(base + 1000) % array.size].value +
+                        array[(base + 2000) % array.size].value +
+                        array[(base + 3000) % array.size].value
+                )
+    }
+
+    private class Box(val value: Int)
+
+    private fun Array<Box>.mix(multiplier: Long = 1L) {
+        for (node in boxes) {
+            val i = indexOf(node)
+            check(i >= 0)
+            val j = (i + node.value * multiplier).mod(lastIndex)
+            check(j >= 0)
+            copyInto(this, minOf(i, j + 1), minOf(i + 1, j), maxOf(i, j + 1))
+            this[j] = node
+        }
+    }
+
+    companion object {
+        private const val MULTIPLIER = 811589153L
+    }
 }
