753d25ac5d9323042b8fb383c79dd025b7d3f9a4
martapanc <marta.panc@gmail.com>
2022-12-22 16:23:36 +0100

    [Day 21] Part 2 - wip

--- src/main/kotlin/aoc2022/day22/Day22.kt
+++ src/main/kotlin/aoc2022/day22/Day22.kt
@@ -7,13 +7,9 @@ fun readInputToMapWithBlanks(path: String): Map<Coord, Char> {
     val map = mutableMapOf<Coord, Char>()
     val lines = readInputLineByLine(path)
     for (y in lines.indices) {
-        if (lines[y].isBlank()) {
-            break
-        }
+        if (lines[y].isBlank()) break
         lines[y].toCharArray().forEachIndexed { x, char ->
-            if (char != ' ') {
-                map[Coord(x, y)] = char
-            }
+            if (char != ' ') map[Coord(x, y)] = char
         }
     }
     return map
@@ -61,15 +57,71 @@ fun part1(path: String): Long {
     return 4 * (pos.x + 1) + 1000L * (pos.y + 1) + facing.value
 }
 
+fun part2(path: String): Long {
+    val map = readInputToMapWithBlanks(path)
+    val instructions = readInputToInstructions(path)
+
+    val deltas = mapOf(Facing.EAST to Coord(1, 0), Facing.WEST to Coord(-1, 0),
+        Facing.NORTH to Coord(0, -1), Facing.SOUTH to Coord(0, 1))
+    var pos = map.toSortedMap(compareBy({ it.y }, { it.x })).firstKey()
+    var facing = Facing.EAST
+    instructions.forEach { instruction ->
+        run forward@ {
+            repeat(instruction.number) {
+                var newPos = Coord(pos.x + deltas[facing]!!.x, pos.y + deltas[facing]!!.y)
+                if (map[newPos] == null) {
+                    val aroundCube = pos.wrapAroundCubeExample(facing)
+                    newPos = aroundCube.pos
+                    facing = aroundCube.facing
+                }
+                if (map[newPos] != null && map[newPos] == '#') { // bump into wall
+                    return@forward
+                }
+                pos = newPos
+            }
+        }
+        if (instruction.rotation != null) {
+            facing = facing.rotate(instruction.rotation)
+        }
+    }
+    return 4 * (pos.x + 1) + 1000L * (pos.y + 1) + facing.value
+}
+
+fun Coord.wrapAroundCubeExample(facing: Facing): PosAndFacing {
+    val x = this.x
+    val y = this.y
+    return when {
+        facing == Facing.NORTH && y == 0 && x in 8 until 12 -> PosAndFacing(Coord(3 - (x - 8), 4), Facing.SOUTH) // Side 1 to 2
+        facing == Facing.NORTH && y == 4 && x in 0 until 4 -> PosAndFacing(Coord((3 - x) + 8, 0), Facing.SOUTH) // Side 2 to 1
+
+        facing == Facing.SOUTH && y == 7 && x in 0 until 4 -> PosAndFacing(Coord((3 - x) + 8, 11), Facing.SOUTH) // Side 2 to 5
+        facing == Facing.SOUTH && y == 11 && x in 8 until 12 -> PosAndFacing(Coord(3 - (x - 8), 7), Facing.SOUTH) // Side 5 to 2
+
+        facing == Facing.EAST && x == 11 && y in 0 until 4 -> PosAndFacing(Coord(15, (3 - y) + 8), Facing.WEST) // 1 to 6
+        facing == Facing.EAST && x == 15 && y in 8 until 12 -> PosAndFacing(Coord(11, 3 - (y - 8)), Facing.WEST) // 6 to 1
+
+        facing == Facing.WEST && x == 0 && y in 4 until 8 -> PosAndFacing(Coord(3 - (y-4) + 12, 11), Facing.NORTH) // 2 to 5
+        facing == Facing.SOUTH && y == 11 && x in 12 until 16 -> PosAndFacing(Coord(0, (3 - (x-12)) + 4), Facing.EAST) // 5 to 2
+
+        facing == Facing.WEST && x == 8 && y in 0 until 4 -> PosAndFacing(Coord(4 + y, 4), Facing.SOUTH) // 1 to 3
+        facing == Facing.NORTH && y == 4 && x in 4 until 8 -> PosAndFacing(Coord(8, x - 4), Facing.EAST) // 3 to 1
+
+        facing == Facing.EAST && x == 11 && y in 4 until 8 -> PosAndFacing(Coord(3 - (y-4) + 12, 8), Facing.SOUTH) // 4 to 6
+        facing == Facing.NORTH && y == 8 && x in 12 until 16 -> PosAndFacing(Coord(11, 3 - (x-12) + 4), Facing.WEST) // 6 to 4
+
+        facing == Facing.SOUTH && y == 7 && x in 4 until 8 -> PosAndFacing(Coord(8, 3 - (x-4) + 8), Facing.EAST) // 3 to 5
+        facing == Facing.WEST && x == 8 && y in 8 until 12 -> PosAndFacing(Coord(3 - (y-8) + 4, 7), Facing.NORTH) // 5 to 3
+
+        else -> PosAndFacing(Coord(0, 0), Facing.NORTH)
+    }
+
+}
+
 private fun Facing.wrapAround(newPos: Coord, map: Map<Coord, Char>) = when (this) {
     Facing.NORTH -> Coord(newPos.x, map.keys.filter { it.x == newPos.x }.maxBy { it.y }.y)
     Facing.EAST -> Coord(map.keys.filter { it.y == newPos.y }.minBy { it.x }.x, newPos.y)
     Facing.SOUTH -> Coord(newPos.x, map.keys.filter { it.x == newPos.x }.minBy { it.y }.y)
     Facing.WEST -> Coord(map.keys.filter { it.y == newPos.y }.maxBy { it.x }.x, newPos.y)
-}
-
-fun part2(list: String): Long {
-    return 0
 }
 
 fun Facing.rotate(rotation: Rotation): Facing {
@@ -86,6 +138,8 @@ class Instruction(val number: Int, val rotation: Rotation?) {
     override fun toString(): String = "$number $rotation"
 }
 
+class PosAndFacing(val pos: Coord, val facing: Facing)
+
 enum class Facing(val value: Int) { NORTH(3), EAST(0), SOUTH(1), WEST(2) }
 
 enum class Rotation(val char: Char) {
@@ -95,3 +149,4 @@ enum class Rotation(val char: Char) {
         fun fromSign(char: Char): Rotation? = Rotation.values().firstOrNull() { char == it.char }
     }
 }
+
--- src/test/kotlin/aoc2022/day22/Day22KtTest.kt
+++ src/test/kotlin/aoc2022/day22/Day22KtTest.kt
@@ -2,6 +2,7 @@ package aoc2022.day22
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import util.Coord
 
 internal class Day22KtTest {
 
@@ -16,8 +17,8 @@ internal class Day22KtTest {
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+        assertEquals(5031, part2(testInput0))
+//        assertEquals(2515, part2(testInput))
     }
 
     @Test
@@ -30,5 +31,29 @@ internal class Day22KtTest {
         assertEquals(Facing.EAST, Facing.NORTH.rotate(Rotation.RIGHT))
         assertEquals(Facing.NORTH, Facing.WEST.rotate(Rotation.RIGHT))
         assertEquals(Facing.WEST, Facing.SOUTH.rotate(Rotation.RIGHT))
+    }
+
+    @Test
+    fun testWrapAroundCubeExample() {
+        assertEquals(Coord(0, 4), Coord(11, 0).wrapAroundCubeExample(Facing.NORTH).pos)
+        assertEquals(Coord(9, 0), Coord(2, 4).wrapAroundCubeExample(Facing.NORTH).pos)
+
+        assertEquals(Coord(10, 11), Coord(1, 7).wrapAroundCubeExample(Facing.SOUTH).pos)
+        assertEquals(Coord(2, 7), Coord(9, 11).wrapAroundCubeExample(Facing.SOUTH).pos)
+
+        assertEquals(Coord(15, 10), Coord(11, 1).wrapAroundCubeExample(Facing.EAST).pos)
+        assertEquals(Coord(11, 3), Coord(15, 8).wrapAroundCubeExample(Facing.EAST).pos)
+
+        assertEquals(Coord(15, 11), Coord(0, 4).wrapAroundCubeExample(Facing.WEST).pos)
+        assertEquals(Coord(0, 6), Coord(13, 11).wrapAroundCubeExample(Facing.SOUTH).pos)
+
+        assertEquals(Coord(5, 4), Coord(8, 1).wrapAroundCubeExample(Facing.WEST).pos)
+        assertEquals(Coord(8, 2), Coord(6, 4).wrapAroundCubeExample(Facing.NORTH).pos)
+
+        assertEquals(Coord(12, 8), Coord(11, 7).wrapAroundCubeExample(Facing.EAST).pos)
+        assertEquals(Coord(11, 6), Coord(13, 8).wrapAroundCubeExample(Facing.NORTH).pos)
+
+        assertEquals(Coord(8, 10), Coord(5, 7).wrapAroundCubeExample(Facing.SOUTH).pos)
+        assertEquals(Coord(7, 7), Coord(8, 8).wrapAroundCubeExample(Facing.WEST).pos)
     }
 }
