480b025dc1b4342407ac4d4664d130d055d74210
martapanc <marta.panc@gmail.com>
2023-12-02 23:08:27 +0100

    D02P1 - complete

--- src/main/kotlin/aoc2023/day02/Day2.kt
+++ src/main/kotlin/aoc2023/day02/Day2.kt
@@ -1,9 +1,66 @@
 package aoc2023.day02
 
-fun part1(list: List<String>): Int {
+fun parse(lines: List<String>): List<Game> {
+    val games = mutableListOf<Game>()
+    lines.forEach { line ->
+        val gameData = line.split(": ")
+        val roundData = gameData[1].split("; ")
+        val gameRounds = mutableListOf<Round>()
+        roundData.forEach { round ->
+            val colors = round.split(", ")
+            val gameRound = Round()
+            colors.forEach { color ->
+                if (color.contains("blue")) {
+                    gameRound.numBlue = extractNum(color)
+                } else if (color.contains("green")) {
+                    gameRound.numGreen = extractNum(color)
+                } else {
+                    gameRound.numRed = extractNum(color)
+                }
+            }
+            gameRounds.add(gameRound)
+        }
+        games.add(Game(extractNum(gameData[0]), gameRounds))
+    }
+    return games
+}
+
+fun part1(games: List<Game>): Int {
+    val bagContent = Round(14, 13, 12)
+    var sum = 0
+    games.forEach { game ->
+        val highestVals = game.findHighestValues()
+        if (highestVals["maxBlue"]!! <= bagContent.numBlue!!
+            && highestVals["maxGreen"]!! <= bagContent.numGreen!!
+            && highestVals["maxRed"]!! <= bagContent.numRed!!) {
+            sum += game.id!!
+        }
+    }
+    return sum
+}
+
+fun part2(list: List<Game>): Int {
     return 0
 }
 
-fun part2(list: List<String>): Int {
-    return 0
+data class Game(val id: Int?, val rounds: List<Round>) {
+    fun findHighestValues(): Map<String, Int> {
+        val maxBlue = rounds.maxOfOrNull { it.numBlue ?: 0 } ?: 0
+        val maxGreen = rounds.maxOfOrNull { it.numGreen ?: 0 } ?: 0
+        val maxRed = rounds.maxOfOrNull { it.numRed ?: 0 } ?: 0
+
+        return mapOf(
+            "maxBlue" to maxBlue,
+            "maxGreen" to maxGreen,
+            "maxRed" to maxRed
+        )
+    }
+}
+
+data class Round(var numBlue: Int? = 0, var numGreen: Int? = 0, var numRed: Int? = 0)
+
+fun extractNum(input: String): Int? {
+    val regex = Regex("\\d+")
+    val matchResult = regex.find(input)
+    return matchResult?.value?.toInt()
 }
--- src/test/kotlin/aoc2023/day02/Day2KtTest.kt
+++ src/test/kotlin/aoc2023/day02/Day2KtTest.kt
@@ -7,17 +7,19 @@ import util.readInputLineByLine
 internal class Day2KtTest {
 
     private val testInput0 = readInputLineByLine("src/main/kotlin/aoc2023/day02/assets/input0")
+    private val games0 = parse(testInput0);
     private val testInput = readInputLineByLine("src/main/kotlin/aoc2023/day02/assets/input")
+    private val games = parse(testInput);
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput0))
-        assertEquals(8085, part1(testInput))
+        assertEquals(8, part1(games0))
+        assertEquals(2449, part1(games))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+//        assertEquals(70, part2(games0))
+//        assertEquals(2515, part2(games))
     }
 }
