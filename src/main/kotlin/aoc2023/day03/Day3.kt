5c14c2f4ccf6330b292b9ea0fba30b03407742f7
martapanc <marta.panc@gmail.com>
2023-12-03 15:09:12 +0100

    D03P1 - WIP

--- src/main/kotlin/aoc2023/day03/Day3.kt
+++ src/main/kotlin/aoc2023/day03/Day3.kt
@@ -1,7 +1,55 @@
 package aoc2023.day03
 
-fun part1(list: List<String>): Int {
-    return 0
+import aoc2018.day15.GameCoord
+
+private val symbols = setOf('*', '+', '-', '=', '#', '$', '/', '@', '%', '&')
+
+fun parse(lines: List<String>): Map<GameCoord, Char> {
+    val engineSchematic = mutableMapOf<GameCoord, Char>()
+    var x = 0
+    var y = 0
+    lines.forEach {
+        it.forEach { char ->
+            engineSchematic[GameCoord(x, y)] = char
+            x++
+        }
+        x = 0
+        y++
+    }
+
+    return engineSchematic
+}
+
+fun part1(schematic: Map<GameCoord, Char>): Int {
+    var sum = 0
+
+    for (y in 0..schematic.keys.maxBy { it.y }.y) {
+        var currentNumber = ""
+        var currentNumNeighbors = mutableSetOf<Char>()
+        for (x in 0..schematic.keys.maxBy { it.x }.x) {
+            val currentCoord = GameCoord(x, y)
+            val currChar = schematic[currentCoord]!!
+            if (!currChar.isDigit()) {
+                continue
+            } else {
+                currentNumber += currChar
+                val neighborCoords = currentCoord.neighbors(false)
+                neighborCoords.forEach {
+                    schematic[it]?.let { it1 -> currentNumNeighbors.add(it1) }
+                }
+                val eastCoord = GameCoord(x + 1, y)
+                if (schematic[eastCoord] != null && !schematic[eastCoord]!!.isDigit()) {
+                    if (currentNumNeighbors.any { symbols.contains(it) }) {
+                        sum += Integer.parseInt(currentNumber)
+                    }
+
+                    currentNumber = ""
+                    currentNumNeighbors = mutableSetOf()
+                }
+            }
+        }
+    }
+    return sum
 }
 
 fun part2(list: List<String>): Int {
--- src/test/kotlin/aoc2023/day03/Day3KtTest.kt
+++ src/test/kotlin/aoc2023/day03/Day3KtTest.kt
@@ -7,17 +7,19 @@ import util.readInputLineByLine
 internal class Day3KtTest {
 
     private val testInput0 = readInputLineByLine("src/main/kotlin/aoc2023/day03/assets/input0")
+    private val testEngineSchematic = parse(testInput0)
     private val testInput = readInputLineByLine("src/main/kotlin/aoc2023/day03/assets/input")
+    private val engineSchematic = parse(testInput)
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput0))
-        assertEquals(8085, part1(testInput))
+        assertEquals(4361, part1(testEngineSchematic))
+        assertEquals(528369, part1(engineSchematic))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+//        assertEquals(70, part2(testInput0))
+//        assertEquals(2515, part2(testInput))
     }
 }
