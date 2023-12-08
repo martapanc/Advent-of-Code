097e64626eafb9b6b02ef2102d014b84f44344b0
martapanc <marta.panc@gmail.com>
2023-12-08 17:11:24 +0100

    Setup Days 6-8

---
+++ src/test/kotlin/aoc2023/day06/Day6KtTest.kt
@@ -0,0 +1,23 @@
+package aoc2023.day06
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import util.readInputLineByLine
+
+internal class Day6KtTest {
+
+    private val testInput0 = readInputLineByLine("src/main/kotlin/aoc2023/day06/assets/input0")
+    private val testInput = readInputLineByLine("src/main/kotlin/aoc2023/day06/assets/input")
+
+    @Test
+    fun testPart1() {
+        assertEquals(157, part1(testInput0))
+        assertEquals(8085, part1(testInput))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(70, part2(testInput0))
+        assertEquals(2515, part2(testInput))
+    }
+}
