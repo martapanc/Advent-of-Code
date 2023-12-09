6a8da4593c2080f9640e6c4f933ca4aa52c0cecd
martapanc <marta.panc@gmail.com>
2023-12-09 19:10:20 +0100

    D05P1: setup

--- src/main/kotlin/aoc2023/day05/Day5.kt
+++ src/main/kotlin/aoc2023/day05/Day5.kt
@@ -1,38 +1,76 @@
 package aoc2023.day05
 
-fun parse(lines: List<String>) {
+fun parse(lines: List<String>): Input {
     val mappings = mutableMapOf<MapType, List<Range>>()
+    val seeds = mutableSetOf<Long>()
+    for (line in lines) {
+        if (line.isEmpty()) {
+            continue
+        }
 
-    lines.forEach { line ->
+        if (line.startsWith("seeds:")) {
+            seeds.addAll(line
+                .replace("seeds: ", "")
+                .split(" ")
+                .map { it.toLong() })
+            continue
+        }
+
+        val split = line.split(": ")
+        val numberGroups = split[1].split(" | ")
+        val ranges = numberGroups.map {
+            val group = it.split(" ").map { str -> str.toLong() }
+            Range(group[0], group[1], group[2])
+        }
         when {
-            line.startsWith("seeds:") -> {}
-            line.startsWith("seed-to-soil") -> {}
-            line.startsWith("soil-to-fertilizer") -> {}
-            line.startsWith("fertilizer-to-water") -> {}
-            line.startsWith("water-to-light") -> {}
-            line.startsWith("light-to-temperature") -> {}
-            line.startsWith("temperature-to-humidity") -> {}
-            line.startsWith("humidity-to-location") -> {}
+            split[0].startsWith("seed-to-soil") -> {
+                mappings[MapType.SEED_TO_SOIL] = ranges
+            }
+            split[0].startsWith("soil-to-fertilizer") -> {
+                mappings[MapType.SOIL_TO_FERTILIZER] = ranges
+            }
+            split[0].startsWith("fertilizer-to-water") -> {
+                mappings[MapType.FERTILIZER_TO_WATER] = ranges
+            }
+            split[0].startsWith("water-to-light") -> {
+                mappings[MapType.WATER_TO_LIGHT] = ranges
+            }
+            split[0].startsWith("light-to-temperature") -> {
+                mappings[MapType.LIGHT_TO_TEMPERATURE] = ranges
+            }
+            split[0].startsWith("temperature-to-humidity") -> {
+                mappings[MapType.TEMPERATURE_TO_HUMIDITY] = ranges
+            }
+            split[0].startsWith("humidity-to-location") -> {
+                mappings[MapType.HUMIDITY_TO_LOCATION] = ranges
+            }
         }
     }
+    return Input(seeds, mappings)
 }
 
-fun part1(list: List<String>): Int {
+fun part1(mappings: Input): Int {
     return 0
 }
 
-fun part2(list: List<String>): Int {
+fun part2(mappings: Input): Int {
     return 0
 }
 
 data class Range(val destRangeStart: Long, val sourceRangeStart: Long, val rangeLength: Long)
 
-enum class MapType {
-    SEED_TO_SOIL,
-    SOIL_TO_FERTILIZER,
-    FERTILIZER_TO_WATER,
-    WATER_TO_LIGHT,
-    LIGHT_TO_TEMPERATURE,
-    TEMPERATURE_TO_HUMIDITY,
-    HUMIDITY_TO_LOCATION
+data class Input(val seeds: Set<Long>, val mappings: Map<MapType, List<Range>>)
+enum class MapType(val key: String) {
+    SEED_TO_SOIL("seed-to-soil"),
+    SOIL_TO_FERTILIZER("soil-to-fertilizer"),
+    FERTILIZER_TO_WATER("fertilizer-to-water"),
+    WATER_TO_LIGHT("water-to-light"),
+    LIGHT_TO_TEMPERATURE("light-to-temperature"),
+    TEMPERATURE_TO_HUMIDITY("temperature-to-humidity"),
+    HUMIDITY_TO_LOCATION("humidity-to-location");
+
+    companion object {
+        fun byKey(key: String): MapType? =
+            values().firstOrNull { key == it.key }
+    }
 }
--- src/test/kotlin/aoc2023/day05/Day5KtTest.kt
+++ src/test/kotlin/aoc2023/day05/Day5KtTest.kt
@@ -7,17 +7,19 @@ import util.readInputLineByLine
 internal class Day5KtTest {
 
     private val testInput0 = readInputLineByLine("src/main/kotlin/aoc2023/day05/assets/input0")
-    private val testInput = readInputLineByLine("src/main/kotlin/aoc2023/day05/assets/input")
+    private val testMapping = parse(testInput0)
+    private val input = readInputLineByLine("src/main/kotlin/aoc2023/day05/assets/input")
+    private val mapping = parse(input)
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput0))
-        assertEquals(8085, part1(testInput))
+        assertEquals(35, part1(testMapping))
+        assertEquals(8085, part1(mapping))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+//        assertEquals(70, part2(testMapping))
+//        assertEquals(2515, part2(mapping))
     }
 }
