0014322bb0868a1b84228b49e523318bc16c7dbd
martapanc <marta.panc@gmail.com>
2023-12-10 18:46:09 +0100

    D08P1: complete

--- src/main/kotlin/aoc2023/day08/Day8.kt
+++ src/main/kotlin/aoc2023/day08/Day8.kt
@@ -1,9 +1,50 @@
 package aoc2023.day08
 
-fun part1(list: List<String>): Int {
+fun parse(lines: List<String>): Network {
+    val input = lines[0]
+    val nodes = mutableMapOf<String, Node>()
+    (2 until lines.size).forEach { i ->
+        val split = lines[i].split(" = ")
+        val directions = split[1]
+            .replace("(", "")
+            .replace(")", "")
+            .trim()
+            .split(", ")
+        nodes[split[0]] = Node(directions[0], directions[1])
+    }
+    return Network(input, nodes)
+}
+
+fun part1(network: Network): Int {
+    var count = 0
+    var zzzWasReached = false
+    var current = "AAA"
+    var i = 0
+
+    while (!zzzWasReached) {
+        if (i == network.input.length) {
+            i = 0
+        }
+        val direction = network.input[i]
+        current = if (direction == 'L') {
+            network.nodes[current]!!.left
+        } else {
+            network.nodes[current]!!.right
+        }
+        count++
+        i++
+        if (current == "ZZZ") {
+            zzzWasReached = true
+        }
+    }
+
+    return count
+}
+
+fun part2(network: Network): Int {
     return 0
 }
 
-fun part2(list: List<String>): Int {
-    return 0
-}
+data class Network(val input: String, val nodes: Map<String, Node>)
+
+data class Node(val left: String, val right: String)
--- src/test/kotlin/aoc2023/day08/Day8KtTest.kt
+++ src/test/kotlin/aoc2023/day08/Day8KtTest.kt
@@ -6,18 +6,25 @@ import util.readInputLineByLine
 
 internal class Day8KtTest {
 
-    private val testInput0 = readInputLineByLine("src/main/kotlin/aoc2023/day08/assets/input0")
-    private val testInput = readInputLineByLine("src/main/kotlin/aoc2023/day08/assets/input")
+    private val testInputLines0 = readInputLineByLine("src/main/kotlin/aoc2023/day08/assets/input0")
+    private val testInput0 = parse(testInputLines0)
+
+    private val testInputLines1 = readInputLineByLine("src/main/kotlin/aoc2023/day08/assets/input1")
+    private val testInput1 = parse(testInputLines1)
+
+    private val inputLines = readInputLineByLine("src/main/kotlin/aoc2023/day08/assets/input")
+    private val input = parse(inputLines)
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput0))
-        assertEquals(8085, part1(testInput))
+        assertEquals(2, part1(testInput0))
+        assertEquals(6, part1(testInput1))
+        assertEquals(19667, part1(input))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+//        assertEquals(70, part2(testInput0))
+//        assertEquals(2515, part2(input))
     }
 }
