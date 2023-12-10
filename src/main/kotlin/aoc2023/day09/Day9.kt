067f3ee076a0b0917089e8b0d9f0349ca18cacfa
martapanc <marta.panc@gmail.com>
2023-12-10 20:07:24 +0100

    D09P1: complete

--- src/main/kotlin/aoc2023/day09/Day9.kt
+++ src/main/kotlin/aoc2023/day09/Day9.kt
@@ -1,9 +1,40 @@
 package aoc2023.day09
 
-fun part1(list: List<String>): Int {
+fun parse(lines: List<String>): List<List<Long>> {
+    val sequences = mutableListOf<List<Long>>()
+    lines.forEach { line ->
+        sequences.add(line.split(" ").map { it.toLong() })
+    }
+    return sequences
+}
+
+fun part1(sequences: List<List<Long>>): Long {
+    var sum = 0L
+    sequences.forEach { sequence ->
+        sum += findNextNumInSequence(sequence)
+    }
+    return sum
+}
+
+fun part2(sequences: List<List<Long>>): Long {
     return 0
 }
 
-fun part2(list: List<String>): Int {
-    return 0
+fun findNextNumInSequence(sequence: List<Long>): Long {
+    val lastItem = reduce(sequence)
+    return sequence[sequence.size - 1] + lastItem
+}
+
+fun reduce(sequence: List<Long>): Long {
+    val reducedSequence = mutableListOf<Long>()
+    (1 until sequence.size).forEach { i ->
+        reducedSequence.add(sequence[i] - sequence[i - 1])
+    }
+
+    if (reducedSequence.all { it == 0L }) {
+        return 0L
+    } else {
+        val lastItem = reduce(reducedSequence)
+        return reducedSequence[reducedSequence.size - 1] + lastItem
+    }
 }
--- src/test/kotlin/aoc2023/day09/Day9KtTest.kt
+++ src/test/kotlin/aoc2023/day09/Day9KtTest.kt
@@ -6,18 +6,41 @@ import util.readInputLineByLine
 
 internal class Day9KtTest {
 
-    private val testInput0 = readInputLineByLine("src/main/kotlin/aoc2023/day09/assets/input0")
-    private val testInput = readInputLineByLine("src/main/kotlin/aoc2023/day09/assets/input")
+    private val testInputLines = readInputLineByLine("src/main/kotlin/aoc2023/day09/assets/input0")
+    private val testInput = parse(testInputLines)
+
+    private val inputLines = readInputLineByLine("src/main/kotlin/aoc2023/day09/assets/input")
+    private val input = parse(inputLines)
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput0))
-        assertEquals(8085, part1(testInput))
+        assertEquals(114, part1(testInput))
+        assertEquals(2043183816, part1(input))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput0))
-        assertEquals(2515, part2(testInput))
+        assertEquals(70, part2(testInput))
+        assertEquals(2515, part2(input))
+    }
+
+    @Test
+    fun testReduce() {
+        assertEquals(0, reduce(listOf(2, 2, 2, 2)))
+        assertEquals(2, reduce(listOf(0, 2, 4, 6)))
+        assertEquals(1, reduce(listOf(1, 2, 3)))
+        assertEquals(3, reduce(listOf(0, 3, 6, 9, 12, 15)))
+        assertEquals(7, reduce(listOf(1, 3, 6, 10, 15, 21)))
+        assertEquals(23, reduce(listOf(10, 13, 16, 21, 30, 45)))
+    }
+
+    @Test
+    fun testFindNextNumInSequence() {
+        assertEquals(2, findNextNumInSequence(listOf(2, 2, 2, 2)))
+        assertEquals(8, findNextNumInSequence(listOf(0, 2, 4, 6)))
+        assertEquals(4, findNextNumInSequence(listOf(1, 2, 3)))
+        assertEquals(18, findNextNumInSequence(listOf(0, 3, 6, 9, 12, 15)))
+        assertEquals(28, findNextNumInSequence(listOf(1, 3, 6, 10, 15, 21)))
+        assertEquals(68, findNextNumInSequence(listOf(10, 13, 16, 21, 30, 45)))
     }
 }
