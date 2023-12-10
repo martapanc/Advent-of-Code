b4f42939303ff60ed8d8e92cefd38f1f5fe4ea89
martapanc <marta.panc@gmail.com>
2023-12-10 20:18:17 +0100

    D09P2: complete

--- src/main/kotlin/aoc2023/day09/Day9.kt
+++ src/main/kotlin/aoc2023/day09/Day9.kt
@@ -17,24 +17,49 @@ fun part1(sequences: List<List<Long>>): Long {
 }
 
 fun part2(sequences: List<List<Long>>): Long {
-    return 0
+    var sum = 0L
+    sequences.forEach { sequence ->
+        sum += findPreviousNumInSequence(sequence)
+    }
+    return sum
 }
 
 fun findNextNumInSequence(sequence: List<Long>): Long {
-    val lastItem = reduce(sequence)
+    val lastItem = reduceToNext(sequence)
     return sequence[sequence.size - 1] + lastItem
 }
 
-fun reduce(sequence: List<Long>): Long {
-    val reducedSequence = mutableListOf<Long>()
-    (1 until sequence.size).forEach { i ->
-        reducedSequence.add(sequence[i] - sequence[i - 1])
-    }
+fun findPreviousNumInSequence(sequence: List<Long>): Long {
+    val firstItem = reduceToPrevious(sequence)
+    return sequence[0] - firstItem
+}
+
+fun reduceToNext(sequence: List<Long>): Long {
+    val reducedSequence = computeDiffSequence(sequence)
 
     if (reducedSequence.all { it == 0L }) {
         return 0L
     } else {
-        val lastItem = reduce(reducedSequence)
+        val lastItem = reduceToNext(reducedSequence)
         return reducedSequence[reducedSequence.size - 1] + lastItem
     }
+}
+
+fun reduceToPrevious(sequence: List<Long>): Long {
+    val reducedSequence = computeDiffSequence(sequence)
+
+    if (reducedSequence.all { it == 0L }) {
+        return 0L
+    } else {
+        val firstItem = reduceToPrevious(reducedSequence)
+        return reducedSequence[0] - firstItem
+    }
+}
+
+private fun computeDiffSequence(sequence: List<Long>): MutableList<Long> {
+    val reducedSequence = mutableListOf<Long>()
+    (1 until sequence.size).forEach { i ->
+        reducedSequence.add(sequence[i] - sequence[i - 1])
+    }
+    return reducedSequence
 }
--- src/test/kotlin/aoc2023/day09/Day9KtTest.kt
+++ src/test/kotlin/aoc2023/day09/Day9KtTest.kt
@@ -20,18 +20,18 @@ internal class Day9KtTest {
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput))
-        assertEquals(2515, part2(input))
+        assertEquals(2, part2(testInput))
+        assertEquals(1118, part2(input))
     }
 
     @Test
-    fun testReduce() {
-        assertEquals(0, reduce(listOf(2, 2, 2, 2)))
-        assertEquals(2, reduce(listOf(0, 2, 4, 6)))
-        assertEquals(1, reduce(listOf(1, 2, 3)))
-        assertEquals(3, reduce(listOf(0, 3, 6, 9, 12, 15)))
-        assertEquals(7, reduce(listOf(1, 3, 6, 10, 15, 21)))
-        assertEquals(23, reduce(listOf(10, 13, 16, 21, 30, 45)))
+    fun testReduceToNext() {
+        assertEquals(0, reduceToNext(listOf(2, 2, 2, 2)))
+        assertEquals(2, reduceToNext(listOf(0, 2, 4, 6)))
+        assertEquals(1, reduceToNext(listOf(1, 2, 3)))
+        assertEquals(3, reduceToNext(listOf(0, 3, 6, 9, 12, 15)))
+        assertEquals(7, reduceToNext(listOf(1, 3, 6, 10, 15, 21)))
+        assertEquals(23, reduceToNext(listOf(10, 13, 16, 21, 30, 45)))
     }
 
     @Test
@@ -42,5 +42,14 @@ internal class Day9KtTest {
         assertEquals(18, findNextNumInSequence(listOf(0, 3, 6, 9, 12, 15)))
         assertEquals(28, findNextNumInSequence(listOf(1, 3, 6, 10, 15, 21)))
         assertEquals(68, findNextNumInSequence(listOf(10, 13, 16, 21, 30, 45)))
+    }
+    @Test
+    fun testFindPreviousNumInSequence() {
+        assertEquals(2, findPreviousNumInSequence(listOf(2, 2, 2, 2)))
+        assertEquals(-2, findPreviousNumInSequence(listOf(0, 2, 4, 6)))
+        assertEquals(0, findPreviousNumInSequence(listOf(1, 2, 3)))
+        assertEquals(-3, findPreviousNumInSequence(listOf(0, 3, 6, 9, 12, 15)))
+        assertEquals(0, findPreviousNumInSequence(listOf(1, 3, 6, 10, 15, 21)))
+        assertEquals(5, findPreviousNumInSequence(listOf(10, 13, 16, 21, 30, 45)))
     }
 }
