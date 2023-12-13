cbf867d0c879740386ab11ca27fba03bee212eec
martapanc <marta.panc@gmail.com>
2023-12-13 19:31:11 +0100

    D11P2: complete

--- src/main/kotlin/aoc2023/day11/Day11.kt
+++ src/main/kotlin/aoc2023/day11/Day11.kt
@@ -3,19 +3,68 @@ package aoc2023.day11
 import util.Coord
 
 fun part1(map: Map<Coord, Char>): Long {
-    val expanded = expand(map)
+    return computeAllDistances(map)
+}
+
+private fun computeAllDistances(map: Map<Coord, Char>, multiplier: Int = 1): Long {
     var sum = 0L
-    val galaxiesCoords = expanded.filter { it.value == '#' }.map { it.key }
+    val galaxiesCoords = map.filter { it.value == '#' }.map { it.key }
+    val emptyCols = findEmptyColumns(map)
+    val emptyRows = findEmptyRows(map)
+
     for ((i1, g1) in galaxiesCoords.withIndex()) {
-        (i1 until galaxiesCoords.size).forEach {i2 ->
-            sum += g1.manhattanDistance(galaxiesCoords[i2])
+        (i1 + 1 until galaxiesCoords.size).forEach { i2 ->
+            val g2 = galaxiesCoords[i2]
+            val emptyCount = countEmptyRowsAndColsBetween(g1, g2, emptyCols, emptyRows)
+            sum += g1.manhattanDistance(g2) + multiplier * emptyCount
         }
     }
     return sum
 }
 
-fun part2(galaxies: Map<Coord, Char>): Long {
-    return 0
+fun part2(map: Map<Coord, Char>, multiplier: Int): Long {
+    return computeAllDistances(map, multiplier)
+}
+
+fun countEmptyRowsAndColsBetween(c1: Coord, c2: Coord, emptyCols: List<Int>, emptyRows: List<Int>): Int {
+    var count = 0
+    val x1 = c1.x
+    val x2 = c2.x
+    val (minX, maxX) = if (x1 < x2) x1 to x2 else x2 to x1
+
+    emptyCols
+        .filter { minX < it && it < maxX }
+        .forEach { count++ }
+
+    val y1 = c1.y
+    val y2 = c2.y
+    val (minY, maxY) = if (y1 < y2) y1 to y2 else y2 to y1
+    emptyRows
+        .filter { minY < it && it < maxY }
+        .forEach { count++ }
+    return count
+}
+
+fun findEmptyColumns(map: Map<Coord, Char>): List<Int> {
+    val emptyCols = mutableListOf<Int>()
+    (0 .. map.keys.maxBy { it.x }.x).forEach { x ->
+        val currentCol = map.entries.filter { it.key.x == x }
+        if (currentCol.all { it.value == '.' }) {
+            emptyCols.add(x)
+        }
+    }
+    return emptyCols
+}
+
+fun findEmptyRows(map: Map<Coord, Char>): List<Int> {
+    val emptyRows = mutableListOf<Int>()
+    (0 .. map.keys.maxBy { it.y }.y).forEach { y ->
+        val currentRow = map.entries.filter { it.key.y == y }
+        if (currentRow.all { it.value == '.' }) {
+            emptyRows.add(y)
+        }
+    }
+    return emptyRows
 }
 
 fun expand(map: Map<Coord, Char>): Map<Coord, Char> {
@@ -35,7 +84,6 @@ fun expand(map: Map<Coord, Char>): Map<Coord, Char> {
             expandedMap[Coord(colId, it.key.y)] = it.value
         }
     }
-
 //    printMap(expandedMap)
     return expandedMap
 }
--- src/test/kotlin/aoc2023/day11/Day11KtTest.kt
+++ src/test/kotlin/aoc2023/day11/Day11KtTest.kt
@@ -2,6 +2,7 @@ package aoc2023.day11
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import util.Coord
 import util.readInputLineByLine
 import util.readInputToMap
 
@@ -19,7 +20,18 @@ internal class Day11KtTest {
 
     @Test
     fun testPart2() {
-//        assertEquals(70, part2(testInput))
-//        assertEquals(2515, part2(input))
+        assertEquals(1030, part2(testInput, 9))
+        assertEquals(8410, part2(testInput, 99))
+        assertEquals(742305960572, part2(input, 999999))
+    }
+
+    @Test
+    fun testCountEmptyRowsAndColsBetween() {
+        val emptyCols = findEmptyColumns(testInput)
+        val emptyRows = findEmptyRows(testInput)
+
+        assertEquals(1, countEmptyRowsAndColsBetween(Coord(3, 0), Coord(0, 2), emptyCols, emptyRows))
+        assertEquals(2, countEmptyRowsAndColsBetween(Coord(3, 0), Coord(1, 5), emptyCols, emptyRows))
+        assertEquals(5, countEmptyRowsAndColsBetween(Coord(0, 2), Coord(9, 9), emptyCols, emptyRows))
     }
 }
