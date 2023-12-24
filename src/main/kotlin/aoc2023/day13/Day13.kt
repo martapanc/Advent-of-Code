f64d12bd33e4c4bf28209164471855ba1271d816
martapanc <marta.panc@gmail.com>
2023-12-24 20:00:25 +0100

    D13P2: complete

--- src/main/kotlin/aoc2023/day13/Day13.kt
+++ src/main/kotlin/aoc2023/day13/Day13.kt
@@ -32,8 +32,12 @@ fun part1(maps: List<Map<Coord, Char>>): Long {
     return sum
 }
 
-fun part2(input: List<Map<Coord, Char>>): Long {
-    return 0
+fun part2(maps: List<Map<Coord, Char>>): Long {
+    var sum = 0L
+    maps.forEach {
+        sum += findImperfectMirror(it)
+    }
+    return sum
 }
 
 fun findMirror(map: Map<Coord, Char>): Int {
@@ -92,11 +96,106 @@ fun findMirror(map: Map<Coord, Char>): Int {
         }
     }
 
-    return -1
+    throw Exception()
+}
+
+fun findImperfectMirror(map: Map<Coord, Char>): Int {
+    val maxX = map.keys.maxBy { it.x }.x
+    val maxY = map.keys.maxBy { it.y }.y
+
+    // Horizontal
+    val groupedByY = map.keys.groupBy { it.y }
+    val sortedYKeys = groupedByY.keys.sorted()
+    val rows = sortedYKeys.map { y ->
+        groupedByY[y]?.sortedBy { it.x }?.map { map[it] }?.joinToString("") ?: ""
+    }
+
+    val consecutiveRowIndices = findConsecutiveAlmostIdenticalIndices(rows)
+    consecutiveRowIndices.forEach { pair ->
+        var isMirror = true
+        var hasSmudgeBeenFound = pair.almostIdentical
+        var a = pair.first - 1
+        var b = pair.second + 1
+        mirror@while (a >= 0 && b <= maxY) {
+            if (rows[a] == rows[b] || isAlmostIdentical(rows[a], rows[b])) {
+                if (isAlmostIdentical(rows[a], rows[b])) {
+                    if (hasSmudgeBeenFound) {
+                        isMirror = false
+                        break@mirror
+                    } else {
+                        hasSmudgeBeenFound = true
+                    }
+                }
+            } else {
+                isMirror = false
+                break@mirror
+            }
+            a--
+            b++
+        }
+
+        if (hasSmudgeBeenFound && isMirror) {
+            return 100 * pair.second
+        }
+    }
+
+    // Vertical
+    val groupedByX = map.keys.groupBy { it.x }
+    val sortedKeys = groupedByX.keys.sorted()
+    val columns = sortedKeys.map { x ->
+        groupedByX[x]?.sortedBy { it.y }?.map { map[it] }?.joinToString("") ?: ""
+    }
+
+    val consecutiveColIndices = findConsecutiveAlmostIdenticalIndices(columns)
+    consecutiveColIndices.forEach { pair ->
+        var isMirror = true
+        var hasSmudgeBeenFound = pair.almostIdentical
+        var a = pair.first - 1
+        var b = pair.second + 1
+        mirror@while (a >= 0 && b <= maxX) {
+            if (columns[a] == columns[b] || isAlmostIdentical(columns[a], columns[b])) {
+                if (isAlmostIdentical(columns[a], columns[b])) {
+                    if (hasSmudgeBeenFound) {
+                        isMirror = false
+                        break@mirror
+                    } else {
+                        hasSmudgeBeenFound = true
+                    }
+                }
+            } else {
+                isMirror = false
+                break@mirror
+            }
+            a--
+            b++
+        }
+
+        if (hasSmudgeBeenFound && isMirror) {
+            return pair.second
+        }
+    }
+
+    throw Exception()
 }
 
 fun equals(list1: List<Pair<Int, Int>>, list2: List<Pair<Int, Int>>): Boolean {
     return list1.size == list2.size && list1.containsAll(list2)
+}
+
+fun isAlmostIdentical(a: String, b: String): Boolean {
+    if (a == b) {
+        return false
+    }
+    var diffCount = 0
+    for (i in a.indices) {
+        if (a[i] != b[i]) {
+            diffCount++
+            if (diffCount > 1) {
+                return false
+            }
+        }
+    }
+    return diffCount == 1
 }
 
 fun findConsecutiveIdenticalIndices(strings: List<String>): List<Pair<Int, Int>> {
@@ -106,5 +205,23 @@ fun findConsecutiveIdenticalIndices(strings: List<String>): List<Pair<Int, Int>>
             consecutiveIdenticalIndices.add(Pair(i, i + 1))
         }
     }
+
     return consecutiveIdenticalIndices
 }
+
+fun findConsecutiveAlmostIdenticalIndices(strings: List<String>):
+        List<StrPair> {
+    val consecutiveIdenticalIndices = mutableListOf<StrPair>()
+    for (i in 0 until strings.size - 1) {
+        if (strings[i] == strings[i + 1]) {
+            consecutiveIdenticalIndices.add(StrPair(i, i + 1, false))
+        }
+        if (isAlmostIdentical(strings[i], strings[i + 1])) {
+            consecutiveIdenticalIndices.add(StrPair(i, i + 1, true))
+        }
+    }
+
+    return consecutiveIdenticalIndices
+}
+
+data class StrPair(val first: Int, val second: Int, val almostIdentical: Boolean)
--- src/test/kotlin/aoc2023/day13/Day13KtTest.kt
+++ src/test/kotlin/aoc2023/day13/Day13KtTest.kt
@@ -1,6 +1,6 @@
 package aoc2023.day13
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import util.readInputLineByLine
 
@@ -9,25 +9,35 @@ internal class Day13KtTest {
     private val testInputLines = readInputLineByLine("src/main/kotlin/aoc2023/day13/assets/input0")
     private val testInput = parse(testInputLines)
 
+    private val testInputLines1 = readInputLineByLine("src/main/kotlin/aoc2023/day13/assets/input1")
+    private val testInput1 = parse(testInputLines1)
+
     private val inputLines = readInputLineByLine("src/main/kotlin/aoc2023/day13/assets/input")
     private val input = parse(inputLines)
 
     @Test
     fun testPart1() {
         assertEquals(405, part1(testInput))
-        assertEquals(35680, part1(input))
-//        assertEquals(35691, part1(input))
+        assertEquals(35691, part1(input))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput))
-        assertEquals(2515, part2(input))
+        assertEquals(400, part2(testInput))
+        assertEquals(10, part2(testInput1))
+        assertEquals(39037, part2(input))
     }
 
     @Test
     fun testFindMirror() {
         assertEquals(5, findMirror(testInput[0]))
         assertEquals(400, findMirror(testInput[1]))
+    }
+
+    @Test
+    fun testIsAlmostIdentical() {
+        assertFalse(isAlmostIdentical("#.##..##.", "#.##..##."))
+        assertTrue(isAlmostIdentical("#.##..##.", "#.##..###"))
+        assertFalse(isAlmostIdentical("#.##..##.", "..##..###"))
     }
 }
