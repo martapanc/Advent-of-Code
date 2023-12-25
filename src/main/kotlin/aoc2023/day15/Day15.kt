adcb6be1135f7101f94bd5362545ea5517872353
martapanc <marta.panc@gmail.com>
2023-12-25 18:09:52 +0100

    D15P1: complete

--- src/main/kotlin/aoc2023/day15/Day15.kt
+++ src/main/kotlin/aoc2023/day15/Day15.kt
@@ -1,17 +1,23 @@
 package aoc2023.day15
 
 fun parse(lines: List<String>): List<String> {
-    val output = mutableListOf<String>()
-    lines.forEach { line ->
-        
-    }
-    return output
+    return lines[0].split(",")
 }
 
-fun part1(input: List<String>): Long {
-    return 0
+fun part1(sequences: List<String>): Int {
+    return sequences.sumOf { hash(it) }
 }
 
 fun part2(input: List<String>): Long {
     return 0
+}
+
+fun hash(inputString: String): Int {
+    var currentValue = 0
+    for (char in inputString) {
+        currentValue += char.code
+        currentValue *= 17
+        currentValue %= 256
+    }
+    return currentValue
 }
--- src/test/kotlin/aoc2023/day15/Day15KtTest.kt
+++ src/test/kotlin/aoc2023/day15/Day15KtTest.kt
@@ -14,13 +14,25 @@ internal class Day15KtTest {
 
     @Test
     fun testPart1() {
-        assertEquals(157, part1(testInput))
-        assertEquals(8085, part1(input))
+        assertEquals(1320, part1(testInput))
+        assertEquals(511215, part1(input))
     }
 
     @Test
     fun testPart2() {
         assertEquals(70, part2(testInput))
         assertEquals(2515, part2(input))
+    }
+
+    @Test
+    fun testHash() {
+        assertEquals(200, hash("H"))
+        assertEquals(153, hash("HA"))
+        assertEquals(172, hash("HAS"))
+        assertEquals(52, hash("HASH"))
+        assertEquals(30, hash("rn=1"))
+        assertEquals(253, hash("cm-"))
+        assertEquals(9, hash("ot=9"))
+        assertEquals(231, hash("ot=7"))
     }
 }
