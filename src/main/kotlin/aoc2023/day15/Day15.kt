7436c8dac33f3149819db2e0c1a0d1fc665d38d1
martapanc <marta.panc@gmail.com>
2023-12-25 18:42:04 +0100

    D15P2: wip

--- src/main/kotlin/aoc2023/day15/Day15.kt
+++ src/main/kotlin/aoc2023/day15/Day15.kt
@@ -1,14 +1,13 @@
 package aoc2023.day15
 
-fun parse(lines: List<String>): List<String> {
-    return lines[0].split(",")
-}
-
-fun part1(sequences: List<String>): Int {
+fun part1(lines: List<String>): Int {
+    val sequences = split(lines)
     return sequences.sumOf { hash(it) }
 }
 
-fun part2(input: List<String>): Long {
+fun part2(lines: List<String>): Long {
+    val sequences = split(lines).map { Sequence(it) }
+//    val boxes = mutableMapOf<Int,
     return 0
 }
 
@@ -20,4 +19,45 @@ fun hash(inputString: String): Int {
         currentValue %= 256
     }
     return currentValue
+}
+
+private fun split(lines: List<String>) = lines[0].split(",")
+
+data class Lens(val label: String, val focalLength: Int)
+
+data class Sequence(val seqString: String) {
+    val label: String by lazy { parseLabel() }
+    val box: Int by lazy { calculateHash() }
+    val opType: OpType by lazy { parseOpType() }
+    val focalLength: Int? by lazy { parseFocalLength() }
+
+    private fun parseLabel(): String = replaceAndSplit()[0]
+
+    private fun parseOpType(): OpType {
+        if (seqString.contains("-")) {
+            return OpType.DASH
+        }
+        return OpType.EQUALS
+    }
+
+    private fun calculateHash(): Int = hash(label)
+
+    private fun parseFocalLength(): Int? {
+        if (seqString.contains("="))
+            return replaceAndSplit()[1].toInt()
+        return null
+    }
+
+    private fun replaceAndSplit() = seqString
+        .replace("-", " ")
+        .replace("=", " ")
+        .split(" ")
+
+    override fun toString(): String {
+        return "{label: '$label', box: $box, opType: $opType, focalLength: $focalLength}"
+    }
+}
+
+enum class OpType {
+    DASH, EQUALS
 }
--- src/test/kotlin/aoc2023/day15/Day15KtTest.kt
+++ src/test/kotlin/aoc2023/day15/Day15KtTest.kt
@@ -7,21 +7,18 @@ import util.readInputLineByLine
 internal class Day15KtTest {
 
     private val testInputLines = readInputLineByLine("src/main/kotlin/aoc2023/day15/assets/input0")
-    private val testInput = parse(testInputLines)
-
     private val inputLines = readInputLineByLine("src/main/kotlin/aoc2023/day15/assets/input")
-    private val input = parse(inputLines)
 
     @Test
     fun testPart1() {
-        assertEquals(1320, part1(testInput))
-        assertEquals(511215, part1(input))
+        assertEquals(1320, part1(testInputLines))
+        assertEquals(511215, part1(inputLines))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(70, part2(testInput))
-        assertEquals(2515, part2(input))
+        assertEquals(145, part2(testInputLines))
+        assertEquals(2515, part2(inputLines))
     }
 
     @Test
