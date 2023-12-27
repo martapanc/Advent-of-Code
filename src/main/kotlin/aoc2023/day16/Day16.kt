002c78a96ca5af69527d52535aedeb0aa4458e6b
martapanc <marta.panc@gmail.com>
2023-12-27 18:44:13 +0100

    D16P1: complete

--- src/main/kotlin/aoc2023/day16/Day16.kt
+++ src/main/kotlin/aoc2023/day16/Day16.kt
@@ -1,17 +1,85 @@
 package aoc2023.day16
 
-fun parse(lines: List<String>): List<String> {
-    val output = mutableListOf<String>()
-    lines.forEach { line ->
-        
+import aoc2022.day08.Cardinal
+import aoc2022.day08.Cardinal.NORTH
+import aoc2022.day08.Cardinal.SOUTH
+import aoc2022.day08.Cardinal.EAST
+import aoc2022.day08.Cardinal.WEST
+import util.Coord
+
+fun part1(map: Map<Coord, Char>): Int {
+    val energizedCells = mutableSetOf<Pos>()
+    var edge = listOf(Pos(Coord(0, 0), EAST))
+
+    while (edge.isNotEmpty()) {
+        val newEdge = mutableListOf<Pos>()
+        loop@for (currPos in edge) {
+            if (energizedCells.contains(currPos)) {
+                continue@loop
+            }
+            val currCell = map[currPos.coord]
+            if (currCell != null) {
+                energizedCells.add(currPos)
+                when (currCell) {
+                    '|' -> {
+                        if (currPos.direction == NORTH || currPos.direction == SOUTH) {
+                            newEdge.add(currPos.moveSameDirection())
+                        } else {
+                            newEdge.add(currPos.moveNorth())
+                            newEdge.add(currPos.moveSouth())
+                        }
+                    }
+                    '-' -> {
+                        if (currPos.direction == EAST || currPos.direction == WEST) {
+                            newEdge.add(currPos.moveSameDirection())
+                        } else {
+                            newEdge.add(currPos.moveEast())
+                            newEdge.add(currPos.moveWest())
+                        }
+                    }
+                    '/' -> {
+                        when (currPos.direction) {
+                            NORTH -> newEdge.add(currPos.moveEast())
+                            EAST -> newEdge.add(currPos.moveNorth())
+                            SOUTH -> newEdge.add(currPos.moveWest())
+                            WEST -> newEdge.add(currPos.moveSouth())
+                        }
+                    }
+                    '\\' -> {
+                        when (currPos.direction) {
+                            NORTH -> newEdge.add(currPos.moveWest())
+                            EAST -> newEdge.add(currPos.moveSouth())
+                            SOUTH -> newEdge.add(currPos.moveEast())
+                            WEST -> newEdge.add(currPos.moveNorth())
+                        }
+                    }
+                    '.' -> newEdge.add(currPos.moveSameDirection())
+                }
+            }
+        }
+        edge = newEdge.toList()
     }
-    return output
+
+    return energizedCells.map { it.coord }.toSet().size
 }
 
-fun part1(input: List<String>): Long {
+fun part2(map: Map<Coord, Char>): Long {
     return 0
 }
 
-fun part2(input: List<String>): Long {
-    return 0
+data class Pos(val coord: Coord, val direction: Cardinal) {
+    fun moveSameDirection(): Pos {
+        val newCoord = when (direction) {
+            NORTH -> Coord(coord.x, coord.y - 1)
+            EAST -> Coord(coord.x + 1, coord.y)
+            SOUTH -> Coord(coord.x, coord.y + 1)
+            WEST -> Coord(coord.x - 1, coord.y)
+        }
+        return Pos(newCoord, direction)
+    }
+
+    fun moveNorth(): Pos = Pos(Coord(coord.x, coord.y - 1), NORTH)
+    fun moveSouth(): Pos = Pos(Coord(coord.x, coord.y + 1), SOUTH)
+    fun moveWest(): Pos = Pos(Coord(coord.x - 1, coord.y), WEST)
+    fun moveEast(): Pos = Pos(Coord(coord.x + 1, coord.y), EAST)
 }
