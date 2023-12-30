fb57fcc6f1f4c769f599264eb945668097ae62a8
martapanc <marta.panc@gmail.com>
2023-12-30 21:04:41 +0100

    D21P2: complete

--- src/main/kotlin/aoc2023/day21/Day21.kt
+++ src/main/kotlin/aoc2023/day21/Day21.kt
@@ -1,6 +1,7 @@
 package aoc2023.day21
 
 import util.Coord
+import kotlin.math.abs
 
 fun part1(map: Map<Coord, Char>, steps: Int = 64): Int {
     val start = map.entries.first { it.value == 'S' }.key
@@ -22,6 +23,113 @@ fun part1(map: Map<Coord, Char>, steps: Int = 64): Int {
     return edge.size
 }
 
-fun part2(map: Map<Coord, Char>, steps: Int = 26501365): Int {
-    return part1(map, steps)
+fun part2(map: Map<Coord, Char>, steps: Int = 26501365): Long {
+    val (height, width) = Pair(map.keys.maxBy { it.y }.y + 1, map.keys.maxBy { it.x }.x + 1)
+    val stepsInt = steps / width
+    val stepsRem = steps.mod(width)
+
+    val (dirX, dirY) = Pair(
+        intArrayOf(1, 0, -1, 0),
+        intArrayOf(0, 1, 0, -1)
+    )
+
+    for (y0 in 0 until height) {
+        for (x0 in 0 until  width) {
+            if (map[Coord(x0, y0)] == 'S') {
+                var queue0 = ArrayDeque<Coord>()
+                var queue1 = ArrayDeque<Coord>()
+                val u = HashSet<Coord>()
+
+                fun enqueue(x: Int, y: Int) {
+                    if (map[Coord(x.mod(width), y.mod(height))] == '#')
+                        return
+                    val coord = Coord(x, y)
+                    if (coord in u)
+                        return
+                    u += coord
+                    queue1.add(Coord(x, y))
+                }
+
+                enqueue(x0, y0)
+
+                fun sumDiamond(xx: Int, yy: Int, sh: Int): Long {
+                    var sum = 0L
+                    val x1 = x0 + xx * width + sh * stepsRem
+                    val y1 = y0 + yy * height + sh * stepsRem
+                    when (sh) {
+                        0 -> for (x in x1 - stepsRem..x1 + stepsRem + stepsRem) {
+                            val w = stepsRem - abs(x - x1)
+                            for (y in y1 - w..y1 + w) if (Coord(x, y) in u) sum++
+                        }
+
+                        1 -> for (x in x1 - stepsRem + 1..x1 + stepsRem) {
+                            val w = if (x <= x1) stepsRem - (x1 - x) else stepsRem - (x - x1 - 1)
+                            for (y in y1 - w + 1..y1 + w) if (Coord(x, y) in u) sum++
+                        }
+                    }
+                    return sum
+                }
+
+                val rc = Array(2) { LongArray(2) }
+
+                fun computed(dw: Int): Long {
+                    var sum = 0L
+                    for (i in -dw..dw) {
+                        val w = dw - abs(i)
+                        val t0 = (abs(i) + w + dw) % 2
+                        sum += rc[t0][0] * (w + 1) + rc[1 - t0][0] * w
+                    }
+                    for (i in -dw..<dw) {
+                        val w = if (i < 0) dw + i + 1 else dw - i
+                        sum += rc[0][1] * w + rc[1][1] * w
+                    }
+                    return sum
+                }
+
+                var cur = 0
+                while (true) {
+                    if (queue0.isEmpty()) {
+                        queue0 = queue1.also { queue1 = queue0 }
+                        if (cur % width == stepsRem) {
+                            val rep = cur / width
+                            for (sh in 0..1) for (ii in -1..0) {
+                                val mod = (ii + rep).mod(2)
+                                if (rc[mod][sh] == 0L) rc[mod][sh] = sumDiamond(ii, 0, sh) else check(
+                                    rc[mod][sh] == sumDiamond(
+                                        ii, 0, sh
+                                    )
+                                )
+                            }
+                            val all = queue0.size.toLong()
+                            val computed = computed(rep)
+                            check(all == computed)
+                            var td = 0L
+                            for (ii in -rep..rep) {
+                                for (jj in -rep..rep) {
+                                    val sd = sumDiamond(ii, jj, 0)
+                                    td += sd
+                                    print(sd.toString().padStart(8))
+                                }
+                                for (jj in -rep..rep) {
+                                    val sd = sumDiamond(ii, jj, 1)
+                                    td += sd
+                                    print(sd.toString().padStart(8))
+                                }
+                                println()
+                            }
+                            check(all == td)
+                            if (rep == 2) {
+                                return computed(stepsInt)
+                            }
+                        }
+                        u.clear()
+                        cur++
+                    }
+                    val (i, j) = queue0.removeFirst()
+                    for (d in 0..3) enqueue(i + dirX[d], j + dirY[d])
+                }
+            }
+        }
+    }
+    return 0L
 }
--- src/test/kotlin/aoc2023/day21/Day21KtTest.kt
+++ src/test/kotlin/aoc2023/day21/Day21KtTest.kt
@@ -2,24 +2,24 @@ package aoc2023.day21
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import util.readInputLineByLine
 import util.readInputToMap
 
 internal class Day21KtTest {
 
-    private val testInput = readInputToMap("src/main/kotlin/aoc2023/day21/assets/input0")
+    private val testInput0 = readInputToMap("src/main/kotlin/aoc2023/day21/assets/input0")
+    private val testInput1 = readInputToMap("src/main/kotlin/aoc2023/day21/assets/input1")
+    private val testInput2 = readInputToMap("src/main/kotlin/aoc2023/day21/assets/input2")
 
     private val input = readInputToMap("src/main/kotlin/aoc2023/day21/assets/input")
 
     @Test
     fun testPart1() {
-        assertEquals(16, part1(testInput, steps = 6))
+        assertEquals(16, part1(testInput0, steps = 6))
         assertEquals(3605, part1(input))
     }
 
     @Test
     fun testPart2() {
-//        assertEquals(16733044, part2(testInput, 5000))
-//        assertEquals(2515, part2(input))
+        assertEquals(596734624269210, part2(input))
     }
 }
