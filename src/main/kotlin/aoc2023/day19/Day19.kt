71bb766a987627891cf2f3d4509ba93fb24e79df
martapanc <marta.panc@gmail.com>
2023-12-30 13:20:26 +0100

    D19P2: refactor and update readme

--- src/main/kotlin/aoc2023/day19/Day19.kt
+++ src/main/kotlin/aoc2023/day19/Day19.kt
@@ -23,54 +23,42 @@ fun part1(input: Day19Input): Long {
 
 private fun workFlow(input: Day19Input, rating: Rating): Int {
     var currentWorkflow = input.workflows["in"]!!
-    var ii = 0
-    main@ while (ii < currentWorkflow.size) {
-        if (evalCheck(currentWorkflow[ii].condition, rating)) {
-            val result = currentWorkflow[ii].dest
-            if (result == "A" || result == "R") {
-                if (result == "A") {
-                    return rating.sum()
-                }
-                break@main
-            }
-            currentWorkflow = input.workflows[result]!!
-            ii = 0
-        } else {
-            ii++
+    while (true) {
+        val ii = currentWorkflow.indexOfFirst { evalCheck(it.condition, rating) }
+        if (ii == -1) {
+            break
+        }
+        when (val result = currentWorkflow[ii].dest) {
+            "A" -> return rating.sum()
+            "R" -> break
+            else -> currentWorkflow = input.workflows[result]!!
         }
     }
     return 0
 }
 
-fun part2(input: Day19Input, max: Int = 4000): Long {
-    val ranges: Map<Char, Range> = mapOf(
-        'x' to Range(1, 4000),
-        'm' to Range(1, 4000),
-        'a' to Range(1, 4000),
-        's' to Range(1, 4000)
-    )
-    return countAccepted(input.workflows, ranges)
+fun part2(input: Day19Input): Long {
+    return countAccepted(input.workflows, "xmas".associateWith { Range(1, 4000) })
 }
 
-fun countAccepted(workflows: Map<String, List<Instruction>>, ranges0: Map<Char, Range>, current: Instruction = Instruction("in")): Long {
-    if (current.condition == null && current.dest == "A") {
-        var prod = 1L
-        ranges0.values.forEach {
-            prod *= it.size()
+fun countAccepted(
+    workflows: Map<String, List<Instruction>>,
+    ranges0: Map<Char, Range>,
+    current: Instruction = Instruction("in")
+): Long {
+    if (current.condition == null) {
+        when (current.dest) {
+            "A" -> return ranges0.values.fold(1L) { acc, range -> acc * range.size() }
+            "R" -> return 0L
         }
-        return prod
-    }
-    if (current.condition == null && current.dest == "R") {
-        return 0L
     }
 
-    val workflow = workflows[current.dest]!!
     val ranges = ranges0.toMutableMap()
     var combinations = 0L
 
-    for (instruction in workflow) {
+    for (instruction in workflows[current.dest]!!) {
         val condition = instruction.condition
-        if (condition == null)  {
+        if (condition == null) {
             combinations += countAccepted(workflows, ranges, Instruction(instruction.dest))
             break
         } else {
@@ -79,8 +67,10 @@ fun countAccepted(workflows: Map<String, List<Instruction>>, ranges0: Map<Char, 
             val comparator = condition.substring(2).toInt()
             val currentRange = ranges[variable]!!
             val (rangeTrue, rangeFalse) = when (op) {
-                '<' -> currentRange.low .. minOf(currentRange.high, comparator - 1) to maxOf(currentRange.low, comparator) .. currentRange.high
-                '>' -> maxOf(currentRange.low, comparator + 1) .. currentRange.high to currentRange.low .. minOf(currentRange.high, comparator)
+                '<' -> currentRange.low..minOf(currentRange.high, comparator - 1) to
+                        maxOf(currentRange.low, comparator)..currentRange.high
+                '>' -> maxOf(currentRange.low, comparator + 1)..currentRange.high to
+                        currentRange.low..minOf(currentRange.high, comparator)
                 else -> throw IllegalArgumentException()
             }
 
@@ -95,7 +85,6 @@ fun countAccepted(workflows: Map<String, List<Instruction>>, ranges0: Map<Char, 
             ranges[variable] = Range(rangeFalse.first, rangeFalse.last)
         }
     }
-
     return combinations
 }
 
@@ -113,9 +102,7 @@ data class Workflow(val input: String) {
             .split(",").map { Instruction(it) }
     }
 
-    private fun parseName(): String {
-        return input.split("{")[0]
-    }
+    private fun parseName(): String = input.split("{")[0]
 }
 
 data class Instruction(val input: String) {
@@ -137,6 +124,7 @@ data class Instruction(val input: String) {
     }
 
 }
+
 fun evalCheck(check: String?, rating: Rating): Boolean {
     if (check == null) {
         return true
@@ -158,25 +146,17 @@ fun evalCheck(check: String?, rating: Rating): Boolean {
 
 data class Rating(val input: String) {
     private val ratings: Map<Char, Int> by lazy { parseRatings() }
-    constructor(x: Int, m: Int, a: Int, s: Int) : this(buildString {
-        append("{x=$x,m=$m,a=$a,s=$s}")
-    })
 
     fun x() = ratings['x']!!
     fun m() = ratings['m']!!
     fun a() = ratings['a']!!
     fun s() = ratings['s']!!
 
-    fun sum(): Int {
-        return x() + m() + a() + s()
-    }
+    fun sum(): Int = x() + m() + a() + s()
 
     private fun parseRatings(): Map<Char, Int> {
         val map = mutableMapOf<Char, Int>()
-        for (rating in input
-            .replace("{", "")
-            .replace("}", "")
-            .split(",")) {
+        for (rating in input.removeSurrounding("{", "}").split(",")) {
             val split = rating.split("=")
             map[split[0][0]] = split[1].toInt()
         }
