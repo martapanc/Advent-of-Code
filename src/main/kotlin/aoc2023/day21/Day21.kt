277fd501501d245be14e87e78db1e670a79b5bb0
martapanc <marta.panc@gmail.com>
2023-12-30 21:13:53 +0100

    D21P2: refactor and readme

--- src/main/kotlin/aoc2023/day21/Day21.kt
+++ src/main/kotlin/aoc2023/day21/Day21.kt
@@ -28,105 +28,115 @@ fun part2(map: Map<Coord, Char>, steps: Int = 26501365): Long {
     val stepsInt = steps / width
     val stepsRem = steps.mod(width)
 
+    // Direction vectors for the four cardinal directions
     val (dirX, dirY) = Pair(
         intArrayOf(1, 0, -1, 0),
         intArrayOf(0, 1, 0, -1)
     )
 
     for (y0 in 0 until height) {
-        for (x0 in 0 until  width) {
+        for (x0 in 0 until width) {
             if (map[Coord(x0, y0)] == 'S') {
                 var queue0 = ArrayDeque<Coord>()
                 var queue1 = ArrayDeque<Coord>()
-                val u = HashSet<Coord>()
+                val visited = HashSet<Coord>()
 
+                // Enqueue a new coordinate if it's valid
                 fun enqueue(x: Int, y: Int) {
                     if (map[Coord(x.mod(width), y.mod(height))] == '#')
                         return
                     val coord = Coord(x, y)
-                    if (coord in u)
+                    if (coord in visited)
                         return
-                    u += coord
+                    visited += coord
                     queue1.add(Coord(x, y))
                 }
 
                 enqueue(x0, y0)
 
-                fun sumDiamond(xx: Int, yy: Int, sh: Int): Long {
+                // Calculate the sum within a diamond shape
+                fun sumDiamond(xx: Int, yy: Int, shape: Int): Long {
                     var sum = 0L
-                    val x1 = x0 + xx * width + sh * stepsRem
-                    val y1 = y0 + yy * height + sh * stepsRem
-                    when (sh) {
+                    val x1 = x0 + xx * width + shape * stepsRem
+                    val y1 = y0 + yy * height + shape * stepsRem
+                    when (shape) {
                         0 -> for (x in x1 - stepsRem..x1 + stepsRem + stepsRem) {
-                            val w = stepsRem - abs(x - x1)
-                            for (y in y1 - w..y1 + w) if (Coord(x, y) in u) sum++
+                            val diff = stepsRem - abs(x - x1)
+                            for (y in y1 - diff..y1 + diff) {
+                                if (Coord(x, y) in visited)
+                                    sum++
+                            }
                         }
 
                         1 -> for (x in x1 - stepsRem + 1..x1 + stepsRem) {
-                            val w = if (x <= x1) stepsRem - (x1 - x) else stepsRem - (x - x1 - 1)
-                            for (y in y1 - w + 1..y1 + w) if (Coord(x, y) in u) sum++
+                            val diff = if (x <= x1) stepsRem - (x1 - x) else stepsRem - (x - x1 - 1)
+                            for (y in y1 - diff + 1..y1 + diff) {
+                                if (Coord(x, y) in visited)
+                                    sum++
+                            }
                         }
                     }
                     return sum
                 }
 
-                val rc = Array(2) { LongArray(2) }
+                val ranges = Array(2) { LongArray(2) }
 
-                fun computed(dw: Int): Long {
+                // Calculate the sum within a specified range
+                fun computed(diff: Int): Long {
                     var sum = 0L
-                    for (i in -dw..dw) {
-                        val w = dw - abs(i)
-                        val t0 = (abs(i) + w + dw) % 2
-                        sum += rc[t0][0] * (w + 1) + rc[1 - t0][0] * w
+                    for (i in -diff..diff) {
+                        val w = diff - abs(i)
+                        val t0 = (abs(i) + w + diff) % 2
+                        sum += ranges[t0][0] * (w + 1) + ranges[1 - t0][0] * w
                     }
-                    for (i in -dw..<dw) {
-                        val w = if (i < 0) dw + i + 1 else dw - i
-                        sum += rc[0][1] * w + rc[1][1] * w
+                    for (i in -diff until diff) {
+                        val w = if (i < 0) diff + i + 1 else diff - i
+                        sum += ranges[0][1] * w + ranges[1][1] * w
                     }
                     return sum
                 }
 
-                var cur = 0
+                var current = 0
                 while (true) {
                     if (queue0.isEmpty()) {
                         queue0 = queue1.also { queue1 = queue0 }
-                        if (cur % width == stepsRem) {
-                            val rep = cur / width
-                            for (sh in 0..1) for (ii in -1..0) {
-                                val mod = (ii + rep).mod(2)
-                                if (rc[mod][sh] == 0L) rc[mod][sh] = sumDiamond(ii, 0, sh) else check(
-                                    rc[mod][sh] == sumDiamond(
-                                        ii, 0, sh
+                        if (current % width == stepsRem) {
+                            val rep = current / width
+                            for (sh in 0..1) {
+                                for (xx in -1..0) {
+                                    val mod = (xx + rep).mod(2)
+                                    if (ranges[mod][sh] == 0L) ranges[mod][sh] = sumDiamond(xx, 0, sh) else check(
+                                        ranges[mod][sh] == sumDiamond(
+                                            xx, 0, sh
+                                        )
                                     )
-                                )
+                                }
                             }
                             val all = queue0.size.toLong()
                             val computed = computed(rep)
                             check(all == computed)
                             var td = 0L
-                            for (ii in -rep..rep) {
-                                for (jj in -rep..rep) {
-                                    val sd = sumDiamond(ii, jj, 0)
+                            for (yy in -rep..rep) {
+                                for (xx in -rep..rep) {
+                                    val sd = sumDiamond(xx, yy, 0)
                                     td += sd
-                                    print(sd.toString().padStart(8))
                                 }
-                                for (jj in -rep..rep) {
-                                    val sd = sumDiamond(ii, jj, 1)
+                                for (xx in -rep..rep) {
+                                    val sd = sumDiamond(xx, yy, 1)
                                     td += sd
-                                    print(sd.toString().padStart(8))
                                 }
-                                println()
                             }
                             check(all == td)
                             if (rep == 2) {
                                 return computed(stepsInt)
                             }
                         }
-                        u.clear()
-                        cur++
+                        visited.clear()
+                        current++
                     }
-                    val (i, j) = queue0.removeFirst()
-                    for (d in 0..3) enqueue(i + dirX[d], j + dirY[d])
+                    val (x, y) = queue0.removeFirst()
+                    for (d in 0..3)
+                        enqueue(x + dirX[d], y + dirY[d])
                 }
             }
         }
