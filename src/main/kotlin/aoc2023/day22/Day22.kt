ae5c4d491ffe8ec06675406e938825340b5eb03e
martapanc <marta.panc@gmail.com>
2024-01-01 19:54:53 +0100

    D22P1: wip

--- src/main/kotlin/aoc2023/day22/Day22.kt
+++ src/main/kotlin/aoc2023/day22/Day22.kt
@@ -1,6 +1,7 @@
 package aoc2023.day22
 
 import aoc2022.day18.Coord3d
+import util.Coord
 
 fun parse(lines: List<String>): List<Brick> {
     val bricks = mutableListOf<Brick>()
@@ -19,6 +20,8 @@ fun parse(lines: List<String>): List<Brick> {
 }
 
 fun part1(bricks: List<Brick>): Long {
+    val maxX = (bricks.map { it.a.x } + bricks.map { it.b.x }).maxBy { it }
+    val maxY = (bricks.map { it.a.y } + bricks.map { it.b.y }).maxBy { it }
     return 0
 }
 
@@ -26,5 +29,68 @@ fun part2(bricks: List<Brick>): Long {
     return 0
 }
 
-data class Brick(val a: Coord3d, val b: Coord3d) {
+fun brickCanFall(brick: Brick, bricks: List<Brick>): Boolean {
+    val bricksBottom = brick.bottomZ()
+    // Brick's already on bottom
+    if (bricksBottom == 1) {
+        return false
+    }
+    // More than 1 layer above the highest
+    if ((bricksBottom - bricks[bricks.lastIndex].bottomZ()) > 1) {
+        return true
+    }
+
+    val bricksInLowerLayer = bricks.filter { it.occupiesLayerZ(bricksBottom - 1) }
+
+    val cellsOccupiedInLowerLayer = cellsOccupiedOnLayer(bricksInLowerLayer)
+    val brickCoordsInLayer = brick.getCoordinatesOnLayer()
+
+    val intersect = cellsOccupiedInLowerLayer.intersect(brickCoordsInLayer)
+    return intersect.isEmpty()
 }
+
+fun cellsOccupiedOnLayer(bricksOccupyingLayer: List<Brick>): Set<Coord> {
+    val cellsOccupied = mutableSetOf<Coord>()
+    bricksOccupyingLayer.forEach { brick ->
+        if (brick.a.z != brick.b.z) {
+            cellsOccupied.add(Coord(brick.a.x, brick.a.y))
+        } else {
+            if (brick.a.x == brick.b.x) { // Same X
+                (brick.a.y .. brick.b.y).forEach { y ->
+                    cellsOccupied.add(Coord(brick.a.x, y))
+                }
+            } else { // Same y
+                (brick.a.x .. brick.b.x).forEach { x ->
+                    cellsOccupied.add(Coord(x, brick.a.y))
+                }
+            }
+        }
+    }
+    return cellsOccupied
+}
+
+data class Brick(val a: Coord3d, val b: Coord3d) {
+    fun bottomZ(): Int = minOf(a.z, minOf(b.z))
+
+    fun occupiesLayerZ(z: Int): Boolean = z in IntRange(a.z, b.z) || z in IntRange(b.z, a.z)
+
+    fun getCoordinatesOnLayer(): Set<Coord> {
+        val coords = mutableSetOf<Coord>()
+        if (a.z != b.z) {
+            coords.add(Coord(a.x, a.y))
+        } else {
+            if (a.x == b.x) {
+                (a.y .. b.y).forEach { y ->
+                    coords.add(Coord(a.x, y))
+                }
+            } else {
+                (a.x .. b.x).forEach { x ->
+                    coords.add(Coord(x, a.y))
+                }
+            }
+        }
+        return coords
+    }
+}
+
+
--- src/test/kotlin/aoc2023/day22/Day22KtTest.kt
+++ src/test/kotlin/aoc2023/day22/Day22KtTest.kt
@@ -1,7 +1,9 @@
 package aoc2023.day22
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import aoc2022.day18.Coord3d
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import util.Coord
 import util.readInputLineByLine
 
 internal class Day22KtTest {
@@ -22,5 +24,43 @@ internal class Day22KtTest {
     fun testPart2() {
         assertEquals(70, part2(testInput))
         assertEquals(2515, part2(input))
+    }
+
+    @Test
+    fun testBrickCanFall() {
+        val brickOnBottom = Brick(Coord3d(1, 1, 1), Coord3d(1, 2, 1))
+
+        assertFalse(brickCanFall(Brick(Coord3d(0, 0, 1), Coord3d(0, 3, 1)), listOf(brickOnBottom)))
+        assertTrue(brickCanFall(Brick(Coord3d(0, 0, 3), Coord3d(0, 3, 3)), listOf(brickOnBottom)))
+        assertTrue(brickCanFall(Brick(Coord3d(0, 0, 2), Coord3d(1, 0, 2)), listOf(brickOnBottom)))
+        assertFalse(brickCanFall(Brick(Coord3d(0, 1, 2), Coord3d(1, 1, 2)), listOf(brickOnBottom)))
+    }
+
+    @Test
+    fun testBrick() {
+        val testBrick = Brick(Coord3d(1, 1, 3), Coord3d(1, 1, 1))
+        assertFalse(testBrick.occupiesLayerZ(0))
+        assertTrue(testBrick.occupiesLayerZ(1))
+        assertTrue(testBrick.occupiesLayerZ(2))
+        assertTrue(testBrick.occupiesLayerZ(3))
+        assertFalse(testBrick.occupiesLayerZ(4))
+    }
+
+    @Test
+    fun testCellsOccupiedOnLayer() {
+        val bricks = listOf(
+            Brick(Coord3d(0, 0, 1), Coord3d(0, 2, 1)), // 1
+            Brick(Coord3d(1, 1, 1), Coord3d(2, 1, 1)), // 2
+            Brick(Coord3d(2, 2, 1), Coord3d(2, 2, 3))  // 3
+        )
+        val result = setOf(
+            Coord(0, 0), // 1
+            Coord(0, 1), // 1
+            Coord(0, 2), // 1
+            Coord(1, 1), // 2
+            Coord(2, 1), // 2
+            Coord(2, 2), // 3
+        )
+        assertEquals(result, cellsOccupiedOnLayer(bricks))
     }
 }
