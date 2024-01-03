659d604f3f98703709c709deeaf33cd9bc05224d
martapancaldi <marta.pancaldi17@gmail.com>
2024-01-03 19:52:05 +0100

    Setup days 23-25

---
+++ src/main/kotlin/aoc2023/day23/Day23.kt
@@ -0,0 +1,17 @@
+package aoc2023.day23
+
+fun parse(lines: List<String>): List<String> {
+    val output = mutableListOf<String>()
+    lines.forEach { line ->
+        
+    }
+    return output
+}
+
+fun part1(input: List<String>): Long {
+    return 0
+}
+
+fun part2(input: List<String>): Long {
+    return 0
+}
---
+++ src/test/kotlin/aoc2023/day23/Day23KtTest.kt
@@ -0,0 +1,26 @@
+package aoc2023.day23
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import util.readInputLineByLine
+
+internal class Day23KtTest {
+
+    private val testInputLines = readInputLineByLine("src/main/kotlin/aoc2023/day23/assets/input0")
+    private val testInput = parse(testInputLines)
+
+    private val inputLines = readInputLineByLine("src/main/kotlin/aoc2023/day23/assets/input")
+    private val input = parse(inputLines)
+
+    @Test
+    fun testPart1() {
+        assertEquals(157, part1(testInput))
+        assertEquals(8085, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(70, part2(testInput))
+        assertEquals(2515, part2(input))
+    }
+}
