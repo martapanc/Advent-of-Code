6611bcc789075543b868ec83f21842a9b3c42422
martapanc <marta.panc@gmail.com>
2024-11-30 17:37:27 +0100

    Setup 2024 project

--- 2024/utils/setupDay.mjs
+++ 2024/utils/setupDay.mjs
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
 import commandLineArgs from 'command-line-args'
 
 const optionDefinitions = [
     { name: 'year', alias: 'y', type: Number, defaultValue: new Date().getFullYear() },
-    { name: 'day', type: Number, defaultValue: new Date().getDate() },
+    { name: 'day', alias: 'd', type: Number, defaultValue: new Date().getDate() },
 ]
 
 const fetchInput = async () => {
@@ -16,24 +16,33 @@ const fetchInput = async () => {
 
     const options = commandLineArgs(optionDefinitions);
 
-    let day = options.day;
+    const day = options.day;
     if (day < 1 || day > 25) {
         throw new Error('Invalid day');
     }
-    day = day.toString().padStart(2, '0');
+    const formattedDay = day.toString().padStart(2, '0');
 
     const year = options.year;
     if (year < 2015 || year > new Date().getFullYear()) {
         throw new Error('Invalid year');
     }
 
-    const dirName = `src/${year}/day${day}`;
-    if (!await fs.promises.access(dirName)) {
-        await fs.promises.mkdir(dirName, { recursive: true })
+    const dirName = `src/${year}/day${formattedDay}`;
+
+    try {
+        await fs.promises.access(dirName);
+        throw new Error(`Directory ${dirName} already exists, exiting program.`);
+    } catch (err) {
+        if (err.code === 'ENOENT') { // Directory doesn't exist
+            await fs.promises.mkdir(dirName, { recursive: true });
+        } else {
+            console.error(err.message);
+            process.exit(1);
+        }
     }
 
     const url = `https://adventofcode.com/${year}/day/${day}/input`;
-    const inputPath = path.join(__dirname, `../src/${dirName}/input`);
+    const inputPath = path.join(`${dirName}/input`);
 
     const response = await axios.get(url, { headers: { Cookie: `session=${session}` } });
     fs.writeFileSync(inputPath, response.data.trim());
