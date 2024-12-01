97721a7e0c7d6a1ffb53c3be3374383b362f55d0
martapanc <marta.panc@gmail.com>
2024-12-01 10:15:04 +0100

    2024D01: part 2

--- 2024/src/2024/day01/day01.spec.ts
+++ 2024/src/2024/day01/day01.spec.ts
@@ -1,4 +1,4 @@
-import {part1} from "./day01";
+import {part1, part2} from "./day01";
 
 describe('2024 Day 1', () => {
 
@@ -8,8 +8,8 @@ describe('2024 Day 1', () => {
     });
 
     test('Part 2', async () => {
-        // expect(await part2('testInput2')).toEqual(281);
-        // expect(await part2('input')).toEqual(55413);
+        expect(await part2('testInput1')).toEqual(31);
+        expect(await part2('input')).toEqual(29379307);
     });
 });
 
--- 2024/src/2024/day01/day01.ts
+++ 2024/src/2024/day01/day01.ts
@@ -24,3 +24,28 @@ export async function part1(inputFile: string): Promise<number> {
 
     return distanceChecksum;
 }
+
+export async function part2(inputFile: string): Promise<number> {
+    const inputPath = path.join(__dirname, inputFile);
+    const lines = await readInputLineByLine(inputPath);
+
+    const leftList: number[] = [];
+    const rightList: number[] = [];
+
+    lines.forEach(line => {
+        const numbers = line.split(/\s+/).map(str => Number.parseInt(str));
+        leftList.push(numbers[0]);
+        rightList.push(numbers[1]);
+    });
+
+    leftList.sort();
+    rightList.sort();
+
+    let totalSimilarityScore = 0;
+    leftList.forEach((num, index) => {
+        const occurrences = rightList.filter(rightNum => rightNum === num).length;
+        totalSimilarityScore += num * occurrences;
+    });
+
+    return totalSimilarityScore;
+}
