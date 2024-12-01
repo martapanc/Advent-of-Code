a05564cb8a8f8fdd9c14694fd5f41fde33afac24
martapanc <marta.panc@gmail.com>
2024-12-01 10:21:26 +0100

    2024D01: refactor

--- 2024/src/2024/day01/day01.ts
+++ 2024/src/2024/day01/day01.ts
@@ -2,6 +2,14 @@ import path from "node:path";
 import {readInputLineByLine} from "@utils/io";
 
 export async function part1(inputFile: string): Promise<number> {
+    return await day1(inputFile, calcDistanceChecksum);
+}
+
+export async function part2(inputFile: string): Promise<number> {
+    return await day1(inputFile, calcSimilarityScore);
+}
+
+async function day1(inputFile: string, calcFn: (list1: number[], list2: number[]) => number): Promise<number> {
     const inputPath = path.join(__dirname, inputFile);
     const lines = await readInputLineByLine(inputPath);
 
@@ -17,35 +25,24 @@ export async function part1(inputFile: string): Promise<number> {
     leftList.sort();
     rightList.sort();
 
+    return calcFn(leftList, rightList);
+}
+
+function calcSimilarityScore(leftList: number[], rightList: number[]): number {
+    let totalSimilarityScore = 0;
+    leftList.forEach((num) => {
+        const occurrences = rightList.filter(rightNum => rightNum === num).length;
+        totalSimilarityScore += num * occurrences;
+    });
+
+    return totalSimilarityScore;
+}
+
+function calcDistanceChecksum(leftList: number[], rightList: number[]): number {
     let distanceChecksum = 0;
     leftList.forEach((num, index) => {
         distanceChecksum += Math.abs(num - rightList[index]);
     });
 
     return distanceChecksum;
-}
-
-export async function part2(inputFile: string): Promise<number> {
-    const inputPath = path.join(__dirname, inputFile);
-    const lines = await readInputLineByLine(inputPath);
-
-    const leftList: number[] = [];
-    const rightList: number[] = [];
-
-    lines.forEach(line => {
-        const numbers = line.split(/\s+/).map(str => Number.parseInt(str));
-        leftList.push(numbers[0]);
-        rightList.push(numbers[1]);
-    });
-
-    leftList.sort();
-    rightList.sort();
-
-    let totalSimilarityScore = 0;
-    leftList.forEach((num, index) => {
-        const occurrences = rightList.filter(rightNum => rightNum === num).length;
-        totalSimilarityScore += num * occurrences;
-    });
-
-    return totalSimilarityScore;
 }
