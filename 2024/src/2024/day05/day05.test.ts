99166755573b1aa2ee44545422dc37964f5d4488
martapanc <marta.panc@gmail.com>
2024-12-05 19:56:50 +0100

    2024D05: Part 2

--- 2024/src/2024/day05/day05.test.ts
+++ 2024/src/2024/day05/day05.test.ts
@@ -1,4 +1,4 @@
-import {part1, part2} from "./day05";
+import {part1, part2, topologicalSort} from "./day05";
 
 describe('2024 Day 5', () => {
     test('Part 1', async () => {
@@ -7,7 +7,12 @@ describe('2024 Day 5', () => {
     });
 
     test('Part 2', async () => {
-        // expect(await part2('testInput1')).toEqual(31);
-        // expect(await part2('input')).toEqual(29379307);
+        expect(await part2('testInput1')).toEqual(123);
+        expect(await part2('input')).toEqual(6319);
     });
+});
+
+test('Topological sort', () => {
+    expect(topologicalSort([61,13,29], [{a: 61, b: 13}, {a: 61, b: 29}, {a: 29, b: 13}])).toEqual([61, 29, 13]);
+    expect(topologicalSort([29, 61, 13], [{a: 61, b: 13}, {a: 61, b: 29}, {a: 29, b: 13}])).toEqual([61, 29, 13]);
 });
--- 2024/src/2024/day05/day05.ts
+++ 2024/src/2024/day05/day05.ts
@@ -6,7 +6,7 @@ export async function part1(inputFile: string) {
 }
 
 export async function part2(inputFile: string) {
-    return await day5(inputFile);
+    return await day5(inputFile, sortIncorrectUpdatesAndCalcChecksum);
 }
 
 type QueueRule = {
@@ -63,4 +63,73 @@ function calcCorrectUpdateChecksum(rules: QueueRule[], queues: Queue[]) {
     });
 
     return checksum;
+}
+
+function sortIncorrectUpdatesAndCalcChecksum(rules: QueueRule[], queues: Queue[]) {
+    let checksum = 0;
+
+    queues.forEach(queue => {
+        let isQueueValid = true;
+        const rulesToCheck = rules.filter(rule => queue.includes(rule.a) && queue.includes(rule.b));
+
+        for (const ruleToCheck of rulesToCheck) {
+            const aIndex = queue.indexOf(ruleToCheck.a);
+            const bIndex = queue.indexOf(ruleToCheck.b);
+            if (aIndex > bIndex) {
+                isQueueValid = false;
+                break;
+            }
+        }
+
+        if (!isQueueValid) {
+            const sortedQueue = topologicalSort(queue, rulesToCheck);
+            checksum += sortedQueue[Math.floor(sortedQueue.length / 2)];
+        }
+    });
+
+    return checksum;
+}
+
+export function topologicalSort(numbers: Queue, rules: QueueRule[]) {
+    const adjList = new Map();
+    const inDegree = new Map();
+
+    numbers.forEach(num => {
+        adjList.set(num, []);
+        inDegree.set(num, 0);
+    });
+
+    rules.forEach(rule => {
+        adjList.get(rule.a).push(rule.b);
+        inDegree.set(rule.b, (inDegree.get(rule.b) ?? 0) + 1);
+    });
+
+    const queue: number[] = [];
+    const result: number[] = [];
+
+    inDegree.forEach((degree, node) => {
+        if (degree === 0) {
+            queue.push(node);
+        }
+    });
+
+    while (queue.length > 0) {
+        const node = queue.shift();
+        if (node)
+            result.push(node);
+
+        adjList.get(node).forEach((neighbor: number) => {
+            inDegree.set(neighbor, inDegree.get(neighbor) - 1);
+
+            if (inDegree.get(neighbor) === 0) {
+                queue.push(neighbor);
+            }
+        });
+    }
+
+    if (result.length === numbers.length) {
+        return result;
+    } else {
+        throw new Error("The graph has a cycle, no valid ordering exists.");
+    }
 }
