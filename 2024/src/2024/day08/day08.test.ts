2b2c220b22c80ed6b01cf575318ee0393a664586
martapanc <marta.panc@gmail.com>
2024-12-08 11:43:29 +0100

    2024D08: part 2

--- 2024/src/2024/day08/day08.test.ts
+++ 2024/src/2024/day08/day08.test.ts
@@ -8,8 +8,8 @@ describe('2024 Day 8', () => {
     });
 
     test('Part 2', async () => {
-        // expect(await part2('testInput1')).toEqual(31);
-        // expect(await part2('input')).toEqual(29379307);
+        expect(await part2('testInput1')).toEqual(34);
+        expect(await part2('input')).toEqual(1359);
     });
 });
 
--- 2024/src/2024/day08/day08.ts
+++ 2024/src/2024/day08/day08.ts
@@ -4,20 +4,20 @@ import {Coord, Grid, readLinesToGrid} from "@utils/grid";
 import {generatePairs} from "@utils/numbers";
 
 export async function part1(inputFile: string) {
-    return await day8(inputFile, findUniqueAntinodes);
-}
-
-export async function part2(inputFile: string) {
     return await day8(inputFile);
 }
 
-async function day8(inputFile: string, calcFn?: (grid: Map<string, string>) => number) {
-    const inputPath = path.join(__dirname, inputFile);
-    const lines = await readInputLineByLine(inputPath);
-    return calcFn?.(readLinesToGrid(lines));
+export async function part2(inputFile: string) {
+    return await day8(inputFile, true);
 }
 
-function findUniqueAntinodes(grid: Map<string, string>) {
+async function day8(inputFile: string, isPart2 = false) {
+    const inputPath = path.join(__dirname, inputFile);
+    const lines = await readInputLineByLine(inputPath);
+    return findUniqueAntinodes(readLinesToGrid(lines), isPart2 ? lines.length : undefined);
+}
+
+function findUniqueAntinodes(grid: Map<string, string>, max?: number) {
     let uniqueAntinodes = new Set<string>();
 
     const antennasMap = new Map<string, string[]>;
@@ -28,12 +28,16 @@ function findUniqueAntinodes(grid: Map<string, string>) {
             } else {
                 antennasMap.set(cell, [coord]);
             }
+
+            if (max) { // Part 2: add antennas as antinodes
+                uniqueAntinodes.add(coord);
+            }
         }
     }
 
     for (let coords of antennasMap.values()) {
         for (let pair of generatePairs(coords)) {
-            const antinodes = findAntinodes(Coord.deserialize(pair[0]), Coord.deserialize(pair[1]));
+            const antinodes = findAntinodes(Coord.deserialize(pair[0]), Coord.deserialize(pair[1]), max);
             for (let antinode of antinodes) {
                 if (grid.has(antinode.serialize())) {
                     uniqueAntinodes.add(antinode.serialize());
@@ -45,12 +49,50 @@ function findUniqueAntinodes(grid: Map<string, string>) {
     return uniqueAntinodes.size;
 }
 
-export function findAntinodes(antennaA: Coord, antennaB: Coord) {
+export function findAntinodes(antennaA: Coord, antennaB: Coord, max?: number) {
     const xDelta = antennaA.x - antennaB.x;
     const yDelta = antennaA.y - antennaB.y;
 
-    const antinodeA = new Coord(antennaA.x + xDelta, antennaA.y + yDelta);
-    const antinodeB = new Coord(antennaB.x - xDelta, antennaB.y - yDelta);
+    let antinodes: Coord[] = [];
 
-    return [antinodeA, antinodeB];
+    let sourceA = antennaA;
+    let sourceB = antennaB;
+
+    let antinodeA = new Coord(sourceA.x + xDelta, sourceA.y + yDelta);
+    let antinodeB = new Coord(sourceB.x - xDelta, sourceB.y - yDelta);
+
+    let antinodeAWithinGrid = true;
+    let antinodeBWithinGrid = true;
+
+    if (max) {
+        while (antinodeAWithinGrid || antinodeBWithinGrid) {
+            antinodes.push(antinodeA);
+            antinodes.push(antinodeB);
+
+            sourceA = new Coord(antinodeA.x, antinodeA.y);
+            sourceB = new Coord(antinodeB.x, antinodeB.y);
+
+            antinodeA = new Coord(sourceA.x + xDelta, sourceA.y + yDelta);
+            antinodeB = new Coord(sourceB.x - xDelta, sourceB.y - yDelta);
+
+            if (!isWithinRange(antinodeA, max)) {
+                antinodeAWithinGrid = false;
+            }
+            if (!isWithinRange(antinodeB, max)) {
+                antinodeBWithinGrid = false;
+            }
+        }
+    } else {
+        antinodes.push(antinodeA);
+        antinodes.push(antinodeB);
+    }
+
+    return antinodes;
+}
+
+function isWithinRange(coord: Coord, max: number) {
+    const { x, y } = coord;
+
+    const min = 0;
+    return x >= min && x < max && y >= min && y < max;
 }
