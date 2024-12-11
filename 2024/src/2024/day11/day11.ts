30a29c2bf610cdc89c10310129f8225eeebc2069
martapanc <marta.panc@gmail.com>
2024-12-11 20:21:32 +0100

    2024D11: Part 2

--- 2024/src/2024/day11/day11.ts
+++ 2024/src/2024/day11/day11.ts
@@ -1,10 +1,9 @@
-
 export async function part1(inputString: string) {
-    return await day11(inputString, 25);
+    return day11(inputString, 25);
 }
 
 export async function part2(inputString: string) {
-    return await day11(inputString, 75);
+    return day11(inputString, 75);
 }
 
 async function day11(inputString: string, times: number) {
@@ -12,21 +11,39 @@ async function day11(inputString: string, times: number) {
 }
 
 function calcBlinkOutput(stones: string[], times: number) {
-    let expandedStonesCount= 0;
+    let stoneCounts: Map<string, number> = new Map();
 
     stones.forEach(stone => {
-        expandedStonesCount += blinkNtimes([stone], times);
-    })
+        stoneCounts.set(stone, (stoneCounts.get(stone) || 0) + 1);
+    });
 
-    return expandedStonesCount;
+    for (let i = 0; i < times; i++) {
+        stoneCounts = blink(stoneCounts);
+    }
+
+    return Array.from(stoneCounts.values())
+        .reduce((sum, count) => sum + count, 0);
 }
 
-function blinkNtimes(stones: string[], times: number) {
-    let expandedStones = [...stones];
-    for (let i = 0; i < times; i++) {
-        expandedStones = blink(expandedStones);
-    }
-    return expandedStones.length;
+function blink(stoneCounts: Map<string, number>): Map<string, number> {
+    const newStoneCounts: Map<string, number> = new Map();
+
+    stoneCounts.forEach((count, stone) => {
+        if (stone === '0') {
+            newStoneCounts.set('1', (newStoneCounts.get('1') || 0) + count);
+        } else if (stone.length % 2 === 0) {
+            const half = stone.length / 2;
+            const first = Number.parseInt(stone.slice(0, half)) + '';
+            const second = Number.parseInt(stone.slice(half)) + '';
+            newStoneCounts.set(first, (newStoneCounts.get(first) || 0) + count);
+            newStoneCounts.set(second, (newStoneCounts.get(second) || 0) + count);
+        } else {
+            const multiplied = (Number.parseInt(stone) * 2024) + '';
+            newStoneCounts.set(multiplied, (newStoneCounts.get(multiplied) || 0) + count);
+        }
+    });
+
+    return newStoneCounts;
 }
 
 // Every time you blink, the stones each simultaneously change according to the _first applicable_ rule in this list:
@@ -36,7 +53,7 @@ function blinkNtimes(stones: string[], times: number) {
 //    The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone.
 // 3. If none of the other rules apply, the stone is replaced by a new stone;
 //    the old stone's number multiplied by 2024 is engraved on the new stone.
-export function blink(stones: string[] | string): string[] {
+export function blinkV1(stones: string[] | string): string[] {
     const inputStones = typeof stones === 'string' ? stones.split(" ") : stones;
     const updatedStones: string[] = [];
 
