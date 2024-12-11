000c9804f6a2e14f30cdb69ce7f10bb05c43ede3
martapanc <marta.panc@gmail.com>
2024-12-11 19:56:00 +0100

    2024D11: Part 1

---
+++ 2024/src/2024/day11/day11.test.ts
@@ -0,0 +1,24 @@
+import {blink, part1, part2} from "./day11";
+
+const input = "41078 18 7 0 4785508 535256 8154 447";
+const testInput1 = "0 1 10 99 999";
+const testInput2 = "125 17";
+
+describe('2024 Day 11', () => {
+    test('Part 1', async () => {
+        expect(await part1(testInput2)).toEqual(55312);
+        expect(await part1(input)).toEqual(217443);
+    });
+
+    test('Part 2', async () => {
+        // expect(await part1(testInput2)).toEqual(55312);
+        // expect(await part2(input)).toEqual(217443);
+    });
+});
+
+test('blink', () => {
+    expect(blink(testInput1).join(" ")).toEqual("1 2024 1 0 9 9 2021976")
+    expect(blink(testInput2).join(" ")).toEqual("253000 1 7")
+    expect(blink(blink(testInput2)).join(" ")).toEqual("253 0 2024 14168")
+    expect(blink(blink(blink(testInput2))).join(" ")).toEqual("512072 1 20 24 28676032")
+});
---
+++ 2024/src/2024/day11/day11.ts
@@ -0,0 +1,52 @@
+import path from "node:path";
+import {readInputLineByLine} from "@utils/io";
+
+export async function part1(inputString: string) {
+    return await day11(inputString, 25);
+}
+
+export async function part2(inputString: string) {
+    return await day11(inputString, 75);
+}
+
+async function day11(inputString: string, times: number) {
+    return calcBlinkOutput(inputString.split(" "), times);
+}
+
+function calcBlinkOutput(stones: string[], times: number) {
+    let expandedStones = [...stones];
+
+    for (let i = 0; i < times; i++) {
+        expandedStones = blink(expandedStones);
+    }
+
+    return expandedStones.length;
+}
+
+// Every time you blink, the stones each simultaneously change according to the _first applicable_ rule in this list:
+//
+// 1. If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+// 2. If the stone is engraved with a number that has an even number of digits, it is replaced by two stones.
+//    The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone.
+// 3. If none of the other rules apply, the stone is replaced by a new stone;
+//    the old stone's number multiplied by 2024 is engraved on the new stone.
+export function blink(stones: string[] | string): string[] {
+    const inputStones = typeof stones === 'string' ? stones.split(" ") : stones;
+    const updatedStones: string[] = [];
+
+    inputStones.forEach((stone, index) => {
+        if (stone === '0') {
+            updatedStones.push("1");
+        } else if (stone.length % 2 === 0) {
+            const half = stone.length / 2;
+            const asList = stone.split("");
+            const first = Number.parseInt(asList.slice(0, half).join("")) + '';
+            const second = Number.parseInt(asList.slice(half).join("")) + '';
+            updatedStones.push(first, second);
+        } else {
+            const asNum = Number.parseInt(stone);
+            updatedStones.push((asNum * 2024) + '');
+        }
+    });
+    return updatedStones
+}
