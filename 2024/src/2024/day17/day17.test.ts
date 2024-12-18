6f429bed0224f158e1483a865d8c31a5b760122e
martapanc <marta.panc@gmail.com>
2024-12-18 19:31:02 +0100

    2024D17: part 1

--- 2024/src/2024/day17/day17.test.ts
+++ 2024/src/2024/day17/day17.test.ts
@@ -2,12 +2,14 @@ import {part1, part2} from "./day17";
 
 describe('2024 Day 17', () => {
     test('Part 1', async () => {
-        expect(await part1('testInput1')).toEqual(11);
-        expect(await part1('input')).toEqual(3246517);
+        expect(await part1({ regA: 10, regB: 0, regC: 0, program: "5,0,5,1,5,4"})).toEqual("0,1,2");
+        expect(await part1({ regA: 2024, regB: 0, regC: 0, program: "0,1,5,4,3,0"})).toEqual("4,2,5,6,7,7,7,7,3,1,0");
+        expect(await part1({ regA: 729, regB: 0, regC: 0, program: "0,1,5,4,3,0"})).toEqual("4,6,3,5,6,3,5,2,1,0");
+        expect(await part1({ regA: 64012472, regB: 0, regC: 0, program: "2,4,1,7,7,5,0,3,1,7,4,1,5,5,3,0"})).toEqual("1,0,2,0,5,7,2,1,3");
     });
 
     test('Part 2', async () => {
-        expect(await part2('testInput1')).toEqual(31);
-        expect(await part2('input')).toEqual(29379307);
+        // expect(await part2('testInput1')).toEqual(31);
+        // expect(await part2('input')).toEqual(29379307);
     });
 });
--- 2024/src/2024/day17/day17.ts
+++ 2024/src/2024/day17/day17.ts
@@ -1,17 +1,83 @@
 import path from "node:path";
 import {readInputLineByLine} from "@utils/io";
 
-export async function part1(inputFile: string) {
-    return await day17(inputFile);
+type Computer = { regA: number, regB: number, regC: number, program: string };
+
+export async function part1(computer: Computer) {
+    return await day17(computer, calcOutput);
 }
 
-export async function part2(inputFile: string) {
-    return await day17(inputFile);
+export async function part2(computer: Computer) {
+    return await day17(computer);
 }
 
-async function day17(inputFile: string, calcFn?: (lines: string[]) => number) {
-    const inputPath = path.join(__dirname, inputFile);
-    const lines = await readInputLineByLine(inputPath);
-
-    return calcFn?.(lines);
+async function day17(computer: Computer, calcFn?: (computer: Computer) => string) {
+    return calcFn?.(computer);
 }
+
+function calcOutput(computer: Computer) {
+    let { regA, regB, regC } = computer;
+    const program = computer.program.split(",").map(s => Number.parseInt(s));
+
+    let output = "";
+
+    function getCombo(operand: number) {
+        if (operand >= 0 && operand <= 3) {
+            return operand;
+        }
+        if (operand === 4) return regA;
+        if (operand === 5) return regB;
+        if (operand === 6) return regC;
+        if (operand === 7) throw new Error("Invalid operand 7");
+    }
+
+    let i = 0;
+    while (i < program.length) {
+        const opCode = program[i];
+        const operand = program[i + 1];
+
+        switch (opCode) {
+            case 0:
+                regA = adv(regA, Math.pow(2, getCombo(operand)!));
+                break;
+            case 1:
+                regB = bxl(regB, operand);
+                break;
+            case 2:
+                regB = bst(getCombo(operand)!);
+                break;
+            case 3:
+                if (regA !== 0) {
+                    i = operand;
+                    continue;
+                }
+                break;
+            case 4:
+                regB = bxc(regB, regC);
+                break;
+            case 5:
+                output += out(getCombo(operand)!) + ","
+                break;
+            case 6:
+                regB = bdv(regA, Math.pow(2, getCombo(operand)!));
+                break;
+            case 7:
+                regC = cdv(regA, Math.pow(2, getCombo(operand)!));
+                break;
+        }
+        i += 2;
+    }
+    return output.substring(0, output.length - 1);
+}
+
+const adv = (first: number, second: number) => Math.trunc(first / second);
+const bdv = (first: number, second: number) => adv(first, second);
+const cdv = (first: number, second: number) => adv(first, second);
+
+const bxl = (first: number, second: number) => first ^ second;
+
+const bst = (num: number) => num % 8;
+
+const bxc = (regB: number, regC: number) => bxl(regB, regC);
+
+const out = (num: number) => bst(num);
