69de71f5cbf7f7bb3e4caab19d9cd0dfe1832274
martapanc <marta.panc@gmail.com>
2024-12-19 22:55:54 +0100

    2024D18: part 2

--- 2024/src/2024/day18/day18.test.ts
+++ 2024/src/2024/day18/day18.test.ts
@@ -7,11 +7,14 @@ describe('2024 Day 18', () => {
 
     test('Part 1', async () => {
         expect(await part1('testInput1', 7, 12)).toEqual(22);
+        expect(await part1('testInput1', 7, 20)).toEqual(24);
+        expect(await part1('testInput1', 7, 21)).toEqual(-1); // Unreachable end
+
         expect(await part1('input')).toEqual(348);
     });
 
     test('Part 2', async () => {
-        // expect(await part2('testInput1', 6)).toEqual(31);
-        // expect(await part2('input')).toEqual(29379307);
+        expect(await part2('testInput1', 7, 12)).toEqual("6,1");
+        // expect(await part2('input')).toEqual("54,44");
     });
 });
--- 2024/src/2024/day18/day18.ts
+++ 2024/src/2024/day18/day18.ts
@@ -4,17 +4,27 @@ import {Coord, getNeighborCoords, getNeighbors, Grid, printGrid} from "@utils/gr
 import {PriorityQueue} from "@utils/queue";
 
 export async function part1(inputFile: string, side: number = 71, firstCount: number = 1024) {
-    return await day18(inputFile, side, firstCount, findPath);
+    return await day18(inputFile, side, findPath, firstCount);
 }
 
-export async function part2(inputFile: string, side: number = 71) {
-    return await day18(inputFile, side);
+export async function part2(inputFile: string, side: number = 71, firstCount: number = 1024) {
+    return await day18(inputFile, side, findFirstBlockingByte, firstCount);
 }
 
-async function day18(inputFile: string, side: number, firstCount?: number, calcFn?: (grid: Grid, end: Coord) => number) {
+async function day18(inputFile: string, side: number, calcFn: (lines: string[], end: Coord, side: number, firstCount: number) => number | string, firstCount: number) {
     const inputPath = path.join(__dirname, inputFile);
     const lines = await readInputLineByLine(inputPath);
 
+    return calcFn(lines, new Coord(side - 1, side - 1), side, firstCount);
+}
+
+type State = {
+    position: string,
+    length: number,
+    path?: Coord[]
+}
+
+function findPath(lines: string[], end: Coord, side: number, firstCount: number) {
     const grid: Grid = new Map<string, string>;
     for (let y = 0; y < side; y++) {
         for (let x = 0; x < side; x++) {
@@ -26,19 +36,8 @@ async function day18(inputFile: string, side: number, firstCount?: number, calcF
             }
         }
     }
-    return calcFn?.(grid, new Coord(side - 1, side - 1));
-}
 
-type State = {
-    position: string,
-    length: number,
-    path?: Coord[]
-}
-
-function findPath(grid: Grid, end: Coord) {
     let currentPos = new Coord(0, 0);
-
-    printGrid(grid, end, 71, 71);
 
     const queue = new PriorityQueue<State>((a, b) => a.length - b.length);
     queue.enqueue({ position: currentPos.serialize(), length: 0 });
@@ -52,20 +51,42 @@ function findPath(grid: Grid, end: Coord) {
             return length;
         }
 
-        if (visited.has(position))
-            continue;
+        if (visited.has(position)) continue;
         visited.add(position);
 
         for (let n of getNeighborCoords(curr)) {
             const neighbor = n.serialize();
-            if (!visited.has(neighbor) && grid.get(neighbor) === '.') {
-                queue.enqueue({
-                    position: neighbor,
-                    length: length + 1,
-                });
+            if (grid.get(neighbor) === '.') {
+                queue.enqueue({ position: neighbor, length: length + 1 });
             }
         }
     }
 
     return -1;
+}
+
+function findFirstBlockingByte(lines: string[], end: Coord, side: number, firstCount: number) {
+    let count = firstCount + 1;
+    let grid: Grid = new Map<string, string>;
+
+    while (count < lines.length) {
+        for (let y = 0; y < side; y++) {
+            for (let x = 0; x < side; x++) {
+                const coord = `${x},${y}`;
+                if (lines.slice(0, count).some(l => l === coord)) {
+                    grid.set(`{${coord}}`, '#');
+                } else {
+                    grid.set(`{${coord}}`, '.');
+                }
+            }
+        }
+
+        const path = findPath(lines, end, side, count);
+        if (path === -1) {
+            return lines[count - 1];
+        }
+        count++;
+    }
+
+    return "-1";
 }
