f9f84c6f58314aa7f03eaa059772091a5b892cc4
martapanc <marta.panc@gmail.com>
2026-03-16 11:48:15 +0100

    AoC2025: day 2 part 1

--- 2024-2025/src/2025/day02/day02.test.ts
+++ 2024-2025/src/2025/day02/day02.test.ts
@@ -2,12 +2,12 @@ import {part1, part2} from "./day02";
 
 describe('2025 Day 2', () => {
     test('Part 1', async () => {
-        expect(await part1('testInput1')).toEqual(11);
-        expect(await part1('input')).toEqual(3246517);
+        expect(await part1('testInput1')).toEqual(1227775554);
+        expect(await part1('input')).toEqual(19574776074);
     });
 
     test('Part 2', async () => {
-        expect(await part2('testInput1')).toEqual(31);
-        expect(await part2('input')).toEqual(29379307);
+        // expect(await part2('testInput1')).toEqual(31);
+        // expect(await part2('input')).toEqual(29379307);
     });
 });
--- 2024-2025/src/2025/day02/day02.ts
+++ 2024-2025/src/2025/day02/day02.ts
@@ -2,16 +2,47 @@ import path from "node:path";
 import {readInputLineByLine} from "@utils/io";
 
 export async function part1(inputFile: string) {
-    return await day2(inputFile);
+    return await day2(inputFile, findInvalidIds);
 }
 
 export async function part2(inputFile: string) {
     return await day2(inputFile);
 }
 
-async function day2(inputFile: string, calcFn?: (lines: string[]) => number) {
+type NumRange = { from: number; to: number };
+
+async function day2(inputFile: string, calcFn?: (ranges: NumRange[]) => number) {
     const inputPath = path.join(__dirname, inputFile);
     const lines = await readInputLineByLine(inputPath);
 
-    return calcFn?.(lines);
+    const rangeStrings = lines[0].split(',');
+    const ranges: NumRange[] = rangeStrings.map(range => {
+        const res = range.split('-');
+        return {
+            from: Number.parseInt(res[0]),
+            to: Number.parseInt(res[1]),
+        }
+    })
+    return calcFn?.(ranges);
+}
+
+function findInvalidIds(ranges: NumRange[]): number {
+    let invalidIdSum = 0;
+    ranges.forEach(({ from, to }) => {
+
+        for (let n = from; n <= to; n ++) {
+            const numAsString = `${n}`;
+            if (numAsString.length % 2 !== 0)
+                continue;
+
+            const mid = numAsString.length / 2;
+
+            const firstHalf = numAsString.slice(0, mid);
+            const secondHalf = numAsString.slice(mid);
+            if (firstHalf === secondHalf)
+                invalidIdSum += n;
+        }
+    })
+
+    return invalidIdSum;
 }
