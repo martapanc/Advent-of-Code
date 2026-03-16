0496d3f2352a5935a6988b024e250a5196691ec1
martapanc <marta.panc@gmail.com>
2026-03-16 10:50:40 +0100

    AoC2025: day 1 part 1

--- 2024-2025/src/2025/day01/day01.test.ts
+++ 2024-2025/src/2025/day01/day01.test.ts
@@ -2,12 +2,12 @@ import {part1, part2} from "./day01";
 
 describe('2024 Day 1', () => {
     test('Part 1', async () => {
-        expect(await part1('testInput1')).toEqual(11);
-        expect(await part1('input')).toEqual(3246517);
+        expect(await part1('testInput1')).toEqual(3);
+        expect(await part1('input')).toEqual(1105);
     });
 
-    test('Part 2', async () => {
-        expect(await part2('testInput1')).toEqual(31);
-        expect(await part2('input')).toEqual(29379307);
-    });
+    // test('Part 2', async () => {
+    //     expect(await part2('testInput1')).toEqual(31);
+    //     expect(await part2('input')).toEqual(29379307);
+    // });
 });
--- 2024-2025/src/2025/day01/day01.ts
+++ 2024-2025/src/2025/day01/day01.ts
@@ -2,47 +2,42 @@ import path from "node:path";
 import {readInputLineByLine} from "@utils/io";
 
 export async function part1(inputFile: string): Promise<number> {
-    return await day1(inputFile, calcDistanceChecksum);
+    return await day1(inputFile, countZeroPositions);
 }
 
 export async function part2(inputFile: string): Promise<number> {
-    return await day1(inputFile, calcSimilarityScore);
+    return await day1(inputFile, countZeroPositions);
 }
 
-async function day1(inputFile: string, calcFn: (list1: number[], list2: number[]) => number): Promise<number> {
+type MovementType = { dir: string, value: number };
+
+async function day1(inputFile: string, calcFn: ( movements: MovementType[]) => number): Promise<number> {
     const inputPath = path.join(__dirname, inputFile);
     const lines = await readInputLineByLine(inputPath);
 
-    const leftList: number[] = [];
-    const rightList: number[] = [];
+    const movements = lines.map(line => {
+        const dir = line.slice(0, 1);
+        const value = Number.parseInt(line.slice(1));
+        return { dir, value };
+    })
 
-    lines.forEach(line => {
-        const numbers = line.split(/\s+/).map(str => Number.parseInt(str));
-        leftList.push(numbers[0]);
-        rightList.push(numbers[1]);
-    });
-
-    leftList.sort();
-    rightList.sort();
-
-    return calcFn(leftList, rightList);
+    return calcFn(movements);
 }
 
-function calcSimilarityScore(leftList: number[], rightList: number[]): number {
-    let totalSimilarityScore = 0;
-    leftList.forEach((num) => {
-        const occurrences = rightList.filter(rightNum => rightNum === num).length;
-        totalSimilarityScore += num * occurrences;
-    });
 
-    return totalSimilarityScore;
+function countZeroPositions(movements: MovementType[]): number {
+    let zeroPassages = 0;
+    let position = 50;
+
+    movements.forEach(movement => {
+        const newPosition = movement.dir === 'R' ? position + movement.value : position - movement.value;
+        position = ((newPosition % 100) + 100) % 100; // handles all cases: positive, negative, multiple wraps
+
+        if (position === 0) {
+            zeroPassages++;
+        }
+    })
+
+    return zeroPassages;
 }
 
-function calcDistanceChecksum(leftList: number[], rightList: number[]): number {
-    let distanceChecksum = 0;
-    leftList.forEach((num, index) => {
-        distanceChecksum += Math.abs(num - rightList[index]);
-    });
-
-    return distanceChecksum;
-}
